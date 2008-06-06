88545b50056ae60bf8c17ad58e976493afc4d06a
Peter Jones <pjones@pmade.com>
2008-06-06 12:43:19 -0600

    more packages

---
+++ download/erc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Load in common items
+. `dirname $0`/common.sh
+
+URL="http://ftp.gnu.org/gnu/erc/erc-${VERSION}.tar.gz"
+
+file=`fetch_url $URL`
+dir=`untar $file`
+
+(
+  cd $dir || die "WTF"
+  mv Makefile Makefile.org
+  sed -E "s|^PREFIX *=.*|PREFIX=${PREFIX}|" < Makefile.org > Makefile
+  make install || die "make failed"
+)
+
+clean_files $dir
+
