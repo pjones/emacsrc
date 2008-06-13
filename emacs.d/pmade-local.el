7da90ec528d33a879171c2c6cb8386cfcf3db2a8
Peter Jones <pjones@pmade.com>
2008-06-13 16:54:11 -0600

    add new pmade-local.el file

---
+++ emacs.d/pmade-local.el
@@ -0,0 +1,14 @@
+;;; Local settings based on host name
+
+(defun pmade-local-settings ()
+  (let ((host (replace-regexp-in-string "\n" "" (shell-command-to-string "hostname"))))
+    (cond
+     ((string= "skinny.local" host) (pmade-local:skinny)))))
+
+(defun pmade-local:skinny ()
+  "Settings for my MacBook Pro"
+  (let ((frame (selected-frame)))
+    (set-frame-position frame 106 6)
+    (set-frame-size frame 171 54)))
+
+(add-hook 'after-init-hook 'pmade-local-settings)
