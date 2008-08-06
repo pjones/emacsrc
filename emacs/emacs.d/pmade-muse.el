c790a7b97e99aef9dbff25694457b311f397b3b5
Peter Jones <pjones@pmade.com>
2008-08-06 16:32:53 -0600

    Update basecms posting options

--- emacs/emacs.d/pmade-muse.el
+++ emacs/emacs.d/pmade-muse.el
@@ -8,10 +8,21 @@
 (require 'muse-barecms)
 
 ;; Muse Projects
+(setq pmade:test-src-path "~/Develop/pmade/barecms/test/muse"
+      pmade:test-dst-path "~/Develop/tmp/barecms-test"
+      pmade:sc-src-path   "~/Develop/pmade/sc-trunk/doc"
+      pmade:sc-dst-path   "~/Develop/tmp/sc-docs")
+
 (setq muse-project-alist
       `(("pmade.com"
          (,@(muse-project-alist-dirs pmade-com-src) :default "index" :set (muse-barecms-url "http://pmade.com"))
-         ,@(muse-project-alist-styles pmade-com-src pmade-com-dst "barecms" :include "\\.muse$" :exclude "/snippets/"))))
+         ,@(muse-project-alist-styles pmade-com-src pmade-com-dst "barecms" :include "\\.muse$" :exclude "/snippets/"))
+        ("muse-barecms-test"
+         (,@(muse-project-alist-dirs pmade:test-src-path) :default "index" :set (muse-barecms-url "http://localhost:3000" muse-barecms-base-path "/test/"))
+         ,@(muse-project-alist-styles pmade:test-src-path pmade:test-dst-path "barecms" :include "\\.muse$"))
+        ("labs/sva"
+         (,@(muse-project-alist-dirs pmade:sc-src-path) :default "index" :set (muse-barecms-url "http://pmade.com" muse-barecms-base-path "/labs/sva/"))
+         ,@(muse-project-alist-styles pmade:sc-src-path pmade:sc-dst-path "barecms" :include "\\.muse$"))))
 
 (setq pmade-muse-html-header
       "<html>
