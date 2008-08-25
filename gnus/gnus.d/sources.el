9fb4875c18306b9a36a1418bd25ffa6eea2b01b2
Peter Jones <pjones@pmade.com>
2008-08-25 16:46:23 -0600

    Add gmane as a source

--- gnus/gnus.d/sources.el
+++ gnus/gnus.d/sources.el
@@ -8,13 +8,16 @@
 ;; Mail via IMAP and SMTP
 (setq
  pmade-mail-server "mail.pmade.com"
- gnus-secondary-select-methods `((nnimap ,pmade-mail-server (nnimap-stream ssl)))
  nnimap-authinfo-file pmade-authinfo
  message-send-mail-function 'smtpmail-send-it
  smtpmail-smtp-server pmade-mail-server
  smtpmail-auth-credentials pmade-authinfo
  smtpmail-starttls-credentials `((,pmade-mail-server 25 nil nil))
  smtpmail-local-domain "pmade.com")
+
+(setq gnus-secondary-select-methods
+      `((nntp   "news.gmane.org")
+        (nnimap ,pmade-mail-server (nnimap-stream ssl))))
 
 ;; Place sent mail on the server
 (setq gnus-message-archive-group "nnimap+mail.pmade.com:INBOX.Sent")
