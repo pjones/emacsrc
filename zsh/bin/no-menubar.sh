ff5adb1a546cc49f47be8a9136b50699673f207a
Peter Jones <pjones@pmade.com>
2008-09-12 07:19:02 -0600

    Add two new scripts

---
+++ zsh/bin/no-menubar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage:   no-menubar.sh <application-name>"
+  echo "Example: no-menubar.sh iTunes"
+  exit 1
+fi
+
+APP="/Applications/$1.app/Contents"
+
+if [ ! -d $APP ]; then
+  echo "error: $APP doesn't seem to be a directory"
+  exit 1
+fi
+
+defaults write $APP/Info LSUIPresentationMode 4
+echo "done."
