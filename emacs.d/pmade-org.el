db81e8aa41f866bfc9c59507495793dc80d720f0
Peter Jones <pjones@pmade.com>
2009-01-18 17:02:33 -0700

    Completely rework the way I deal with Org

--- emacs.d/pmade-org.el
+++ emacs.d/pmade-org.el
@@ -1,10 +1,5 @@
 ;; This file contains my customizations for the awesome Org-Mode
 
-;; Attempt to load org-mode from a local installation, but if that
-;; fails, ignore the error since an older org-mode comes with emacs.
-(require 'org-install nil t)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-
 ;; Org Invoice
 (autoload 'org-invoice-report "org-invoice" nil t)
 (autoload 'org-dblock-write:invoice "org-invoice" nil t)
@@ -14,38 +9,67 @@
   (let ((eva-conf "~/.comm-sync/etc/eva.el"))
     (when (file-exists-p eva-conf) (load-file eva-conf))))
 
-(add-hook 'org-load-hook
-  (lambda ()
-    (setq
-     ;; General Org Settings
-     org-log-done t
-     org-reverse-note-order t
-     org-deadline-warning-days 14
-     org-hide-leading-stars t
-     org-use-fast-todo-selection t
-     org-special-ctrl-a/e t
-     org-special-ctrl-k t
-     org-M-RET-may-split-line nil
-     org-time-clocksum-format "%02d:%02d"
-     org-completion-use-ido t
+(setq
+ ;; General Org Settings
+ org-log-done t
+ org-reverse-note-order t
+ org-deadline-warning-days 14
+ org-hide-leading-stars t
+ org-use-fast-todo-selection t
+ org-use-fast-tag-selection 'auto
+ org-fast-tag-selection-single-key t
+ org-special-ctrl-a/e t
+ org-special-ctrl-k t
+ org-M-RET-may-split-line nil
+ org-time-clocksum-format "%02d:%02d"
+ org-completion-use-ido t
+ org-tags-exclude-from-inheritance nil
 
-     ;; The notes file and options
-     org-directory "~/Documents/pmade/pmade-inc/planning"
-     org-default-notes-file (concat org-directory "/general/business.org")
-     pmade-org-active-clients (concat (expand-file-name org-directory) "/clients/active")
-     pmade-org-general-files (concat (expand-file-name org-directory) "/general")
+ ;; The notes file and options
+ org-directory "~/Documents/pmade/pmade-inc/planning"
+ org-default-notes-file (concat org-directory "/general/business.org")
+ pmade-org-active-clients (concat (expand-file-name org-directory) "/clients/active")
+ pmade-org-general-files (concat (expand-file-name org-directory) "/general")
 
-     ;; Agenda Files and Agenda Settings
-     org-agenda-window-setup 'current-window
-     org-agenda-ndays 1
-     org-agenda-skip-deadline-if-done t
-     org-agenda-skip-scheduled-if-done t
-     org-agenda-show-all-dates t
-     org-agenda-start-on-weekday 1
-     org-agenda-todo-ignore-with-date t
-     org-agenda-files (append
-                       (directory-files pmade-org-active-clients t "\\.org$")
-                       (directory-files pmade-org-general-files  t "\\.org$")))))
+ ;; Agenda Files and Agenda Settings
+ org-agenda-window-setup 'current-window
+ org-agenda-restore-windows-after-quit nil
+ org-stuck-projects '("LEVEL=2+project/-DONE" ("NEXT" "PENDING") nil "")
+ org-agenda-ndays 1
+ org-agenda-skip-deadline-if-done t
+ org-agenda-skip-scheduled-if-done t
+ org-agenda-show-all-dates t
+ org-agenda-start-on-weekday 1
+ org-agenda-todo-ignore-with-date t
+ org-agenda-files 
+   (append
+    (directory-files pmade-org-active-clients t "\\.org$")
+    (directory-files pmade-org-general-files  t "\\.org$"))
+
+ ;; Custom Agenda Views
+ org-agenda-custom-commands
+ '(("d" "Daily Agenda and Next Actions"
+    ((agenda ""
+             ((org-agenda-todo-keyword-format "")))
+     (tags "LEVEL=2+affirmation"
+           ((org-agenda-remove-tags t)
+            (org-agenda-prefix-format "")
+           (org-agenda-todo-keyword-format "")))
+     (todo "NEXT" 
+           ((org-agenda-sorting-strategy '(tag-up))
+            (org-agenda-show-inherited-tags nil)
+            (org-agenda-todo-keyword-format "")))))
+   ("w" "Weekly Agenda Review"
+    ((agenda "" ((org-agenda-ndays 7)))
+     (todo "PENDING"
+           ((org-agenda-todo-keyword-format "")))
+     (stuck ""))))
+ 
+ ;; Faces
+ org-todo-keyword-faces
+   '(("NEXT"    . pmade-org-next-face)
+     ("PENDING" . pmade-org-pending-face)
+     ("READING" . pmade-org-reading-face)))
 
 (add-hook 'org-mode-hook
   (lambda ()
@@ -60,19 +84,17 @@
     (define-key org-mode-map "\C-\M-n"   'org-metadown)
 
     ;; Buffer Settings
-    (setq
-     save-place nil)
+    (setq save-place nil)
     
     ;; Exporting
-    (setq
-     org-export-html-auto-postamble nil
-     org-export-with-sub-superscripts nil
-     org-export-with-emphasize nil
-     org-icalendar-include-todo nil
-     org-icalendar-store-UID t
-     org-export-html-style-default ""
-     org-export-html-style-extra ""
-     org-export-html-style (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" pmade-print-css "\"/>"))))
+    (setq org-export-html-auto-postamble nil
+          org-export-with-sub-superscripts nil
+          org-export-with-emphasize nil
+          org-icalendar-include-todo nil
+          org-icalendar-store-UID t
+          org-export-html-style-default ""
+          org-export-html-style-extra ""
+          org-export-html-style (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" pmade-print-css "\"/>"))))
 
 ;; Use line highlighting in the Org Agenda
 (add-hook 'org-agenda-mode-hook (lambda () (hl-line-mode 1)))
@@ -95,6 +117,3 @@ the first heading in the export has only one star."
             (forward-line)))))))
 
 (add-hook 'org-export-preprocess-hook 'pmade:org-remove-redundant-heading-markers)
-
-
-
