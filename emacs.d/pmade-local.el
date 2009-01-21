e3281c7adf0ddcebbe37e37104d7f87759394474
Peter Jones <pjones@pmade.com>
2009-01-21 11:08:52 -0700

    Automatically center frame on screen

--- emacs.d/pmade-local.el
+++ emacs.d/pmade-local.el
@@ -6,17 +6,29 @@
      ((string= "skinny.local" host) (pmade-local:skinny))
      ((string= "beefy.local"  host) (pmade-local:beefy)))))
 
+(defun pmade-center-frame (frame)
+  "Move the frame to the center of the screen.  The screen space
+reported by the `display-pixel-height' function will be adjusted
+to account for a missing Mac OS X menu bar."
+  (let* ((display-w (display-pixel-width))
+         (display-h (- (display-pixel-height) 20)) ; remove menu bar
+         (frame-w (frame-pixel-width frame))
+         (frame-h (frame-pixel-height frame))
+         (left (- (/ display-w 2) (/ frame-w 2)))
+         (top  (- (/ display-h 2) (/ frame-h 2))))
+    (set-frame-position frame left top)))
+
 (defun pmade-local:skinny ()
   "Settings for my MacBook Pro"
   (let ((frame (selected-frame)))
-    (set-frame-position frame 106 6)
-    (set-frame-size frame 171 54)))
+    (set-frame-size frame 171 54)
+    (pmade-center-frame frame)))
 
 (defun pmade-local:beefy ()
-  "Settings for my MacBook Pro"
+  "Settings for my Mac Pro"
   (let ((frame (selected-frame)))
-    (set-frame-position frame 116 6)
-    (set-frame-size frame 191 63)))
+    (set-frame-size frame 191 63)
+    (pmade-center-frame frame)))
 
 (defun pmade-local:gui-specific ()
   "Settings for the GUI Emacs."
