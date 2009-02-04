5debe0f7e8bc83b9d99164f80008a78ea40c8483
Peter Jones <pjones@pmade.com>
2009-02-04 14:49:49 -0700

    Start pulling Org from git

--- emacs/download/orgmode.sh
+++ emacs/download/orgmode.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# Load in common items
 . `dirname $0`/common.sh
-URL="http://orgmode.org/org-${VERSION}.tar.gz"
-file=`fetch_url $URL`
-dir=`untar $file`
+URL="git://repo.or.cz/org-mode.git"
+dir="org-mode"
+
+test -d $dir || git clone $URL || die "failed to clone git repo"
+test -d $dir || die "git repo didn't create $dir"
 
 (
   cd $dir || die "WTF"
-  mv Makefile Makefile.org
-  sed $SED_OPTIONS "s|^prefix=.*|prefix=${PREFIX}|" < Makefile.org > Makefile
-  make install || die "make failed"
+  make prefix=${PREFIX} install || die "make failed"
 )
 
 clean_files $dir
