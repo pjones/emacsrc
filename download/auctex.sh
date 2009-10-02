903c01b3757939d32f440a0bedce8e945862b165
Peter Jones <pjones@pmade.com>
2009-10-02 15:31:17 -0600

    Make this work on my laptop

--- download/auctex.sh
+++ download/auctex.sh
@@ -6,14 +6,17 @@
 URL="http://ftp.gnu.org/pub/gnu/auctex/auctex-${VERSION}.tar.gz"
 dir=`fetch_url $URL`
 
+TEXMF=$HOME/Library/texmf
+mkdir -p $TEXMF
+
 (
   cd $dir || die "WTF"
   sh configure --prefix=$PREFIX \
     --with-lispdir=$PREFIX/share/emacs/site-lisp \
-    --with-texmf-dir=$HOME/Library/texmf \
-    > /dev/null 2>&1 || die "configure failed"
-  make > /dev/null 2>&1 || die "make failed"
-  make install > /dev/null 2>&1 || die "make install failed"
+    --with-texmf-dir=$TEXMF \
+    || die "configure failed"
+  make || die "make failed"
+  make install || die "make install failed"
 ) || die "failed to build"
 
 clean_files $dir
