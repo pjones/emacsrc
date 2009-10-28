effb175d69dc1a0bfd19a554ab0908b986840a83
Peter Jones <pjones@pmade.com>
2009-10-28 15:36:51 -0600

    Update of the latex build Makefile and logo style file

--- latex/latex.mk
+++ latex/latex.mk
@@ -3,15 +3,9 @@ LATEX_FILES ?= $(wildcard *.tex)
 all: $(LATEX_FILES:.tex=.pdf)
 
 clean:
-	rm -f $(LATEX_FILES:.tex=.aux)
-	rm -f $(LATEX_FILES:.tex=.log)
-	rm -f $(LATEX_FILES:.tex=.out)
-	rm -f $(LATEX_FILES:.tex=.toc)
-	rm -f $(LATEX_FILES:.tex=.pdf)
+	@for f in $(LATEX_FILES); do latexmk -C $$f; done
 
 .SUFFIXES: .tex .pdf
 
 %.pdf: %.tex
-	pdflatex $<
-	pdflatex $<
-	pdflatex $<
+	latexmk -pdf $<
