45b1963b59196c2a198cef559e1978ca4237bee9
Peter Jones <pjones@pmade.com>
2010-04-14 16:53:13 -0600

    Automatically export SES files as TSV files

--- emacs/emacs.d/pmade-packages.el
+++ emacs/emacs.d/pmade-packages.el
@@ -145,3 +145,23 @@
 (autoload 'muse-mode-choose-mode "muse-mode" "Emacs Muse" t)
 (add-to-list 'auto-mode-alist '("\\.muse$" . muse-mode-choose-mode))
 (eval-after-load "muse-mode" '(load "~/.emacs.d/pmade/pmade-muse"))
+
+;; Simple Emacs Spreadsheet
+(defun pmade:ses-export-buffer-to-tsv nil
+  "Export the current SES buffer to a TSV file.  The file name
+  will be derived from the current buffer name."
+  (interactive)
+  (let* ((fname (or (buffer-file-name) "export"))
+         (bcell (get-text-property (point-min) 'intangible))
+         (ecell (get-text-property (- (point-max) 2) 'intangible))
+         (ses--curcell (cons bcell ecell)))
+    (setq fname
+          (if (string-match "\\..*$" fname)
+              (replace-match ".tsv" nil nil fname)
+            (concat fname ".tsv")))
+    (ses-export-tab nil)
+    (with-temp-file fname (insert (car kill-ring)))))
+  
+(add-hook 'ses-mode-hook
+  (lambda () 
+    (add-hook 'after-save-hook 'pmade:ses-export-buffer-to-tsv)))
