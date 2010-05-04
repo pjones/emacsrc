23f742c4144618690de25b20f83edddc61df59b7
Peter Jones <pjones@pmade.com>
2010-05-04 08:12:32 -0600

    Delete more files, move all files for ~/bin into bin

--- backup/slim.pmade.com/postgresql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BACKUP_HOST=slim.pmade.com
-BACKUP_DIR=postgresql
-. `dirname $0`/../bootstrap.sh
-scp $SCP_OPTIONS pmade.com:/opt/backup/postgresql/\*-latest . || die "scp failed"
--- backup/slim.pmade.com/subversion.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BACKUP_HOST=slim.pmade.com
-BACKUP_DIR=subversion
-. `dirname $0`/../bootstrap.sh
-rsync $RSYNC_OPTIONS pmade.com:/opt/backup/subversion/ . || die "rsync failed"
