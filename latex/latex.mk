ef286d5863383a6fea585d32d0d0157f0784b46e
Peter Jones <pjones@pmade.com>
2011-01-12 15:04:42 -0700

    Add support for building glossaries

--- latex/latex.mk
+++ latex/latex.mk
@@ -1,15 +1,24 @@
 LATEX_FILES ?= $(wildcard *.tex)
+DOT_FILES ?= $(wildcard *.dot)
 LATEST_TEX = $(shell ls -rt *.tex|tail -1)
+ALSO_REMOVE = glg glo gls ist xdy
 
-all: $(LATEX_FILES:.tex=.pdf)
+all: $(DOT_FILES:.dot=.pdf) $(LATEX_FILES:.tex=.pdf)
 
 clean:
 	@for f in $(LATEX_FILES); do latexmk -C $$f; done
+	@for f in $(DOT_FILES:.dot=.pdf); do rm -f $$f; done
+	@rm -f $(foreach f,$(ALSO_REMOVE),*.$(f))
 
-open: $(LATEST_TEX:.tex=.pdf)
-	@open $<
+open: $(DOT_FILES:.dot=.pdf) $(LATEST_TEX:.tex=.pdf)
+	@open $(LATEST_TEX:.tex=.pdf)
 
-.SUFFIXES: .tex .pdf
+.SUFFIXES: .tex .pdf .dot
 
 %.pdf: %.tex
 	latexmk -pdf $<
+
+%.pdf: %.dot
+	dot -Tpdf -o $@ $<
+	pdfcrop $@ $@_crop
+	mv $@_crop $@
