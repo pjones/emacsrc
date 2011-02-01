c5f67f1868136c7b4ac964fb555390327a285778
Peter Jones <pjones@pmade.com>
2011-02-01 16:42:45 -0700

    When ripping streams produces more than one file, join them

--- macosx/scripts/iTunes/ripany.sh
+++ macosx/scripts/iTunes/ripany.sh
@@ -13,7 +13,7 @@ if [ $# -eq 0 ]; then
 fi
 
 case $1 in
-  radio1cz)
+  radio1cz) # http://www.radio1.cz/
     URL="http://netshow.play.cz:8000/radio1.mp3"
     SECONDS=7800
     STATION="Prague Radio 1"
@@ -33,11 +33,21 @@ if [ $# -eq 2 ]; then
 fi
 
 M3U_FILE=$DEST/$1.m3u
-MP3_FILE="$1-"`date +%Y%m%d-%H%M`
+MP3_DIR=$1-`date +%Y%m%d-%H%M`
 
-mkdir -p $DEST
+mkdir -p $DEST/$MP3_DIR
 echo "http://localhost:8000/" >> $M3U_FILE
-streamripper $URL -s --quiet -d $DEST -a $MP3_FILE -l $SECONDS -o never -r
-rm $DEST/$MP3_FILE.cue $M3U_FILE
+streamripper $URL -r -d $DEST/$MP3_DIR -a %q -l $SECONDS -o never -s
+rm $M3U_FILE
 
+COUNT=`ls $DEST/$MP3_DIR/*.mp3|wc -l`
+
+if [ $COUNT -gt 1 ]; then
+  (cd $DEST && mp3wrap $MP3_DIR.mp3 `ls $MP3_DIR/*.mp3|sort`) > /dev/null
+  mv $DEST/${MP3_DIR}_MP3WRAP.mp3 $DEST/$MP3_DIR.mp3
+else
+  cp $DEST/$MP3_DIR/0000.mp3 $DEST/$MP3_DIR.mp3
+fi
+
+rm -r $DEST/$MP3_DIR
 # TODO: add to iTunes and correctly tag items
