e707e1fea1606dc0ef4944c0902fb54b52b3de63
Peter Jones <pjones@pmade.com>
2011-02-06 13:11:34 -0700

    Make Conkeror active first just in case it's not running

--- bin/conkopen.sh
+++ bin/conkopen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# Open and display Conkeror
+osascript -e 'tell application "Conkeror" to activate'
+
 # Open the given URL in Conkeror
 nc 127.0.0.1 4242 > /dev/null <<EOF
 conkeror.url_remoting_fn('$1');
 repl.quit();
 EOF
-
-osascript -e 'tell application "Conkeror" to activate'
