622e953769842e28f375743fb7830d1c509328de
Peter Jones <pjones@pmade.com>
2011-02-09 16:40:31 -0700

    Initial support for TeX4ht

--- latex/latex.mk
+++ latex/latex.mk
@@ -1,23 +1,51 @@
-LATEX_FILES ?= $(wildcard *.tex)
-DOT_FILES ?= $(wildcard *.dot)
-LATEST_TEX = $(shell ls -rt *.tex|tail -1)
-ALSO_REMOVE = glg glo gls ist xdy
+# -*- mode: makefile-gmake -*-
 
+############################################################################
+LATEX_FILES ?= $(wildcard *.tex)
+DOT_FILES   ?= $(wildcard *.dot)
+LATEST_TEX  ?= $(shell ls -rt *.tex|tail -1)
+ALSO_REMOVE  = aux glg glo gls ist xdy 4ct 4tc idv lg
+# SINGLE_PDF = Set to YES for generating a single PDF file
+
+############################################################################
 all: $(DOT_FILES:.dot=.pdf) $(LATEX_FILES:.tex=.pdf)
 
+############################################################################
 clean:
 	@for f in $(LATEX_FILES); do latexmk -C $$f; done
 	@for f in $(DOT_FILES:.dot=.pdf); do rm -f $$f; done
 	@rm -f $(foreach f,$(ALSO_REMOVE),*.$(f))
+	@rm -f $(LATEX_FILES:.tex=.tmp) $(LATEX_FILES:.tex=.html) \
+	       $(LATEX_FILES:.tex=.css) $(LATEX_FILES:.tex=.xref)
 
+############################################################################
+define OPEN_FOR_LATEST_PDF
 open: $(DOT_FILES:.dot=.pdf) $(LATEST_TEX:.tex=.pdf)
-	@open $(LATEST_TEX:.tex=.pdf)
+	@ open $(LATEST_TEX:.tex=.pdf)
+endef
 
+############################################################################
+define OPEN_FOR_SINGLE_PDF
+open: $(LATEX_FILES:.tex=.pdf)
+	@ open $$<
+endef
+
+############################################################################
+$(eval $(call $(if $(SINGLE_PDF),OPEN_FOR_SINGLE_PDF,OPEN_FOR_LATEST_PDF)))
+
+############################################################################
 .SUFFIXES: .tex .pdf .dot
+.PHONY: open clean
 
+############################################################################
 %.pdf: %.tex
 	latexmk -pdf $<
 
+############################################################################
+%.html: %.tex
+	htlatex $< "xhtml"
+
+############################################################################
 %.pdf: %.dot
 	dot -Tpdf -o $@ $<
 	pdfcrop $@ $@_crop
