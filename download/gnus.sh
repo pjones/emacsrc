f3d67fac3da6743324149c413e2e84e81ca3b2df
Peter Jones <pjones@pmade.com>
2011-02-24 16:14:31 -0700

    Upgrade to No Gnus 0.13

--- download/gnus.sh
+++ download/gnus.sh
@@ -6,6 +6,7 @@ dir=`fetch_url $URL`
 
 (
   cd $dir || die "WTF"
+  git checkout $VERSION || die "bad tag: $VERSION"
   sh configure --prefix=$PREFIX --with-emacs > /dev/null 2>&1 || die "configure failed"
   make > /dev/null 2>&1 || die "make failed"
 ) || die "can't build gnus"
