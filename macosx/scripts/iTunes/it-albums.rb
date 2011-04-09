36b254ec31bea6667f565054bfd4e74a68ee4f5e
Peter Jones <pjones@pmade.com>
2011-04-09 13:17:29 -0600

    Add a good description of what this script does

--- macosx/scripts/iTunes/it-albums.rb
+++ macosx/scripts/iTunes/it-albums.rb
@@ -1,6 +1,33 @@
 #!/usr/bin/env ruby
 
 ################################################################################
+#
+# The main purpose of this script is to create a new playlist and
+# populate it with your favorite albums.  It does this by taking an
+# input playlist that should have whole albums in it, and then copying
+# them into a playlist if they have a real album rating (manually set
+# vs. computed by iTunes).
+#
+# Before running this script, select an input playlist that contains
+# whole albums that are candidates for the output playlist.
+#
+# For example, create a smart playlist with these rules:
+#
+#  1. Album Rating is greater than 3 stars
+#  2. Media Kind is Music
+#
+# Select this playlist in iTunes and run this script.  It will weed
+# out albums where iTunes computed the album rating based on the
+# ratings of its tracks, which I find to be faulty because if you only
+# rate a single song (or you have only one song from an album) the
+# album rating will be the same as the song rating.
+#
+# If your input playlist is named "Foo Bar" a new playlist will be
+# created called "Foo Bar (Calculated)".  If the input playlist is
+# named "Foo Bar (Something in parentheses)" the new playlist will be
+# called "Foo Bar (Calculated)".
+#
+################################################################################
 require('rubygems')
 require('appscript')
 
