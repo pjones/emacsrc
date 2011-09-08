ddf7e00b8a5cbdef29405bf090f123b7bdc3abb2
Peter Jones <pjones@pmade.com>
2011-09-08 11:40:36 -0600

    Update some basic macosx settings

--- macosx/after/defaults.sh
+++ macosx/after/defaults.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
 # Global UI changes
+defaults write -g NSRepeatCountBinding -string "^u"
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool TRUE
-defaults write -g AppleScrollBarVariant DoubleBoth
 defaults write -g CheckSpellingWhileTyping -bool TRUE
 defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
 
-# Disable the stupid dashboard
-defaults write com.apple.dashboard mcx-disabled -bool TRUE
-
 # Force Safari to always use tabs, no matter what!
 defaults write com.apple.Safari TargetedClicksCreateTabs -bool TRUE
-
-# X11
-defaults write org.x.X11 wm_ffm -bool TRUE
 
 # The Mac OS X dock application is stupid
 defaults write com.apple.dock static-only -bool TRUE
