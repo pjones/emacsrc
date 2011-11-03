9c9294d3dd5a9ee83f3988eb2e2d1d4de4e8627d
Peter Jones <pjones@pmade.com>
2011-11-03 09:26:08 -0600

    Add .md for Markdown

--- emacs.d/pmade-packages.el
+++ emacs.d/pmade-packages.el
@@ -52,6 +52,7 @@
 
 ;; Markdown Mode
 (autoload 'markdown-mode "markdown-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 ;; Visual Bookmarks
