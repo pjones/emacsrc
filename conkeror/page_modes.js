b87a74a6121a74d946820cbf7d7aca07b89f50b2
Peter Jones <pjones@pmade.com>
2012-04-09 13:26:29 -0600

    Load in the duck duck go page mode

--- conkeror/page_modes.js
+++ conkeror/page_modes.js
@@ -1,6 +1,7 @@
 // Load in the modes
 require("page-modes/google-search-results.js");
 require("page-modes/wikipedia.js");
+require("page-modes/duckduckgo.js");
 
 // Let xkcd-mode put the funny alt text into the page.
 xkcd_add_title = true;
