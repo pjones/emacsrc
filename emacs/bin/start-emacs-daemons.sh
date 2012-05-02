fc24ff21e36d9e654c98f7d6998d0ffb04f37400
Peter Jones <pjones@pmade.com>
2012-05-01 19:26:48 -0600

    Don't output the ssh-agent PID when starting emacs

--- emacs/bin/start-emacs-daemons.sh
+++ emacs/bin/start-emacs-daemons.sh
@@ -17,14 +17,16 @@ if [ $# -eq 0 ]; then
   esac
 fi
 
+# Load SSH agent environment vars
 if [ -r ~/.ssh/agent ]; then
-  . ~/.ssh/agent
+  eval `cat ~/.ssh/agent | egrep -v ^echo`
   export SSH_AGENT_PID
   export SSH_AUTH_SOCK
 fi
 
+# Load GPG agent environment vars
 if [ -r ~/.gpg-agent-info ]; then
-  . ~/.gpg-agent-info
+  eval `cat ~/.gpg-agent-info`
   export GPG_AGENT_INFO
 fi
 
