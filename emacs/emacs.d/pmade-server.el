8a7ac792b86f98c15baf6109413c4f06e3fbb581
Peter Jones <pjones@pmade.com>
2012-05-21 09:23:52 -0600

    Correctly set the Emacs daemon server name on my laptop

--- emacs/emacs.d/pmade-server.el
+++ emacs/emacs.d/pmade-server.el
@@ -4,7 +4,9 @@
     (server-start)))
 
 (setq server-use-tcp t
-      server-host (if (string= "hawkins" system-name)
-                      "10.0.1.10" system-name))
+      server-host (cond
+                   ((string= "hawkins" system-name) "10.0.1.10")
+                   ((string= "seward"  system-name) "192.168.218.1")
+                   (t system-name)))
 
 (add-hook 'after-init-hook 'pmade-server-start)
