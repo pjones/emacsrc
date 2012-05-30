763c315b8513a94fcfeaa25d761a18bf804eefb7
Peter Jones <pjones@pmade.com>
2012-05-30 11:18:15 -0600

    Fix mouse yanking in Emacs

--- emacs/emacs.d/pmade-options.el
+++ emacs/emacs.d/pmade-options.el
@@ -29,6 +29,7 @@
 (setq
  inhibit-startup-message t        ; I've seen it already
  initial-scratch-message nil      ; Ditto
+ mouse-yank-at-point t
  ring-bell-function (lambda ())   ; kill those damn bells
  visible-bell nil                 ; no visual bell
  column-number-mode t             ; display column number in mode line
