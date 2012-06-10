8c6e9aae771d53f01f1b0157a98015ae59369082
Peter Jones <pjones@pmade.com>
2012-06-10 14:24:33 -0600

    Fix path to orgmode not that it goes in its own directory

--- emacs/download/orgmode.sh
+++ emacs/download/orgmode.sh
@@ -2,13 +2,17 @@
 
 . `dirname $0`/common.sh
 URL="git://repo.or.cz/org-mode.git"
+SITE_LISP=$PREFIX/share/emacs/site-lisp
 dir=`fetch_url $URL`
 
 (
   cd $dir || die "WTF"
   git checkout $VERSION || die "bad tag: $VERSION"
   make clean > /dev/null 2>&1
-  make prefix=${PREFIX} install > /dev/null 2>&1 || die "make failed"
+  rm $SITE_LISP/org.el* $SITE_LISP/ob.el* $SITE_LISP/org-*.el* $SITE_LISP/ob-*.el*
+  rm -rf $SITE_LISP/org
+  mkdir -p $SITE_LISP/org
+  make prefix=${PREFIX}/share install > /dev/null 2>&1 || die "make failed"
 )
 
 clean_files $dir
