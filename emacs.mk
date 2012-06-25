456645e3236996b2af1091bfc541c6e216823553
Peter Jones <pjones@pmade.com>
2012-06-25 16:09:09 -0600

    Better error reporting from Emacs

--- emacs.mk
+++ emacs.mk
@@ -1,30 +1,46 @@
+##############################################################################
 SRC	 ?= $(wildcard *.el)
 COMPILED  = $(patsubst %.el,$(DEST)/%.elc,$(SRC))
 PMADE_LOAD_PATH ?= $(CURDIR)/../emacs.d/pmade-loadpath
 
+##############################################################################
 EMACS = emacs
 EMACS_FLAGS = -q --no-site-file --batch
 EMACS_FLAGS += --eval "(add-to-list 'load-path \".\")"
 EMACS_FLAGS += -l $(PMADE_LOAD_PATH)
 EMACS_FLAGS += -f batch-byte-compile
 
+##############################################################################
+EMACS_OUTPUT_FILE = emacs.out
+
+##############################################################################
 .PHONEY: all
 
+##############################################################################
 all: $(DEST) $(COMPILED) $(addprefix $(DEST)/,$(TO_REMOVE))
 	@:
 
+##############################################################################
 uninstall:
 	rm -f $(COMPILED)
 
+##############################################################################
 .SUFFIXES: .el .elc
 
+##############################################################################
 $(DEST)/%.elc: %.el
 	@ echo emacs compile $<
-	@ $(EMACS) $(EMACS_FLAGS) $<
+	@ $(EMACS) $(EMACS_FLAGS) $< > $(EMACS_OUTPUT_FILE) 2>&1
+	@ if [ `egrep -v '^(Wrote|Loading)' $(EMACS_OUTPUT_FILE)|wc -l` -ne 0 ]; then \
+	    cat $(EMACS_OUTPUT_FILE); exit 1; \
+	  fi
+	@ rm $(EMACS_OUTPUT_FILE)
 	@ mv $(<:.el=.elc) $@
 
+##############################################################################
 $(DEST):
 	mkdir -p $@
 
+##############################################################################
 $(addprefix $(DEST)/,$(TO_REMOVE))::
 	@ rm -f $@
