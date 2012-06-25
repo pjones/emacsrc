2ba634388dc59cc5b111b3b79182373db2a93a82
Peter Jones <pjones@pmade.com>
2012-06-25 16:09:25 -0600

    Small clean up

--- emacs/lisp/modes.el
+++ emacs/lisp/modes.el
@@ -1,6 +1,5 @@
 ;;; modes.el -- Load files from ~/.emacs.d/pjones/modes on demand.
-(eval-when-compile
-  (load "./loadpath.el"))
+(eval-when-compile (load "loadpath.el"))
 
 (dolist (file (directory-files pjones:modes-dir t))
   (let ((basename (file-name-sans-extension (file-name-nondirectory file))))
