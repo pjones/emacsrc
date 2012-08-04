7b7605dcc77909afed0141b87ec586a865b5f426
Peter Jones <pjones@pmade.com>
2012-08-03 21:38:30 -0600

    Add a new dired function to only show files that match a regexp

--- modes/dired-conf.el
+++ modes/dired-conf.el
@@ -6,3 +6,15 @@
 (setq dired-listing-switches "-lRA --ignore='.git' --group-directories-first"
       dired-auto-revert-buffer t
       dired-isearch-filenames t)
+
+(defun pjones:dired-show-only-matching-files (regexp)
+  (interactive "sFiles to show (regexp): ")
+  (dired-mark-files-regexp regexp)
+  (dired-toggle-marks)
+  (dired-do-kill-lines))
+
+(defun pjones:dired-load-hook ()
+  (define-key dired-mode-map [?%?h] 'pjones:dired-show-only-matching-files))
+
+(add-hook 'dired-load-hook 'pjones:dired-load-hook)
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
