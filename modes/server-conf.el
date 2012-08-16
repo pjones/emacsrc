66686c33b0cae6b4eb5932b714d5d6da44ef9879
Peter Jones <pjones@pmade.com>
2012-08-16 08:43:44 -0600

    Trying to make my `e' script more generic, and not dependent a specific IP address

--- modes/server-conf.el
+++ modes/server-conf.el
@@ -2,7 +2,4 @@
 (eval-when-compile (require 'server))
 
 (setq server-use-tcp t
-      server-host (cond
-                   ((string= "hawkins" system-name) "10.0.1.10")
-                   ((string= "seward"  system-name) "192.168.218.1")
-                   (t system-name)))
+      server-host "0.0.0.0")
