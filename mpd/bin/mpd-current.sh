865dcd65b50e81e12a2a3ebecf61f0a25806aa8a
Peter Jones <pjones@pmade.com>
2012-08-17 07:52:24 -0600

    Fix an issue where stopping music wouldn't clear xmobar

--- mpd/bin/mpd-current.sh
+++ mpd/bin/mpd-current.sh
@@ -2,24 +2,32 @@
 
 ################################################################################
 # Returns a heavily modified version of `mpc current`.
-mpd_current () {
+current_maybe_empty () {
   mpc current --format '%title% [[(%artist% / %album%)]|==%name%]' | \
     sed -re 's/==([^:-]{,20})[:-]?.*$/(\1)/' -e 's/ *\)/)/g'
 }
 
 ################################################################################
+# Print the current track, or a newline if nothing is playing.  This
+# is necessary because `mpc current' may return nothing, not even a
+# newline, when nothing is playing.
+current () {
+  echo `current_maybe_empty`
+}
+
+################################################################################
 # Sit in a loop and print the current song every time it changes.
-mpd_current_loop () {
-  mpd_current
+current_loop () {
+  current
 
   while mpc idle player > /dev/null 2>&1; do
-    mpd_current
+    current
   done
 }
 
 ################################################################################
 if [ $# -eq 0 -a ! -t 1 ]; then
-  mpd_current_loop
+  current_loop
 else
-  mpd_current
+  current
 fi
