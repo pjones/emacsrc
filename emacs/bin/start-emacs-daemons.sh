6757d78504da0fe6c4c65a477a48d75219562969
Peter Jones <pjones@pmade.com>
2012-08-21 13:03:43 -0600

    New automatic tmux session creation at boot

--- emacs/bin/start-emacs-daemons.sh
+++ emacs/bin/start-emacs-daemons.sh
@@ -17,19 +17,6 @@ if [ $# -eq 0 ]; then
   esac
 fi
 
-# Load SSH agent environment vars
-if [ -r ~/.ssh/agent ]; then
-  eval `cat ~/.ssh/agent | egrep -v ^echo`
-  export SSH_AGENT_PID
-  export SSH_AUTH_SOCK
-fi
-
-# Load GPG agent environment vars
-if [ -r ~/.gpg-agent-info ]; then
-  eval `cat ~/.gpg-agent-info`
-  export GPG_AGENT_INFO
-fi
-
 for name in $daemons; do
   pkill -f daemon=$name
   zsh -c "emacs --daemon=$name" > /dev/null 2>&1
