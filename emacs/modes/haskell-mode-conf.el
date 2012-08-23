ad67eea0194dd35da4ed0b8e8b00acaf88f0aee4
Peter Jones <pjones@pmade.com>
2012-08-23 15:32:10 -0600

    Fix a compile error with my Haskell settings in Emacs

--- emacs/modes/haskell-mode-conf.el
+++ emacs/modes/haskell-mode-conf.el
@@ -1,4 +1,7 @@
 ;;; haskell-mode-conf.el -- Settings for Haskell mode.
-(eval-when-compile (require 'haskell-mode))
+(eval-when-compile
+  (load "../lisp/code.el")
+  (require 'haskell-mode))
+
 (pjones:add-programming-hook 'haskell-mode-hook)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
