93aae2526e4bd2f2d11052e2b7e015e488b9583d
Peter Jones <pjones@pmade.com>
2012-08-23 14:21:53 -0600

    More configuration for Haskell

--- modes/haskell-mode-conf.el
+++ modes/haskell-mode-conf.el
@@ -1,3 +1,4 @@
 ;;; haskell-mode-conf.el -- Settings for Haskell mode.
 (eval-when-compile (require 'haskell-mode))
+(pjones:add-programming-hook 'haskell-mode-hook)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
