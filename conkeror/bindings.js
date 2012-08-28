1a93325776c6ae619d85239a0336cb06d40682fb
Peter Jones <pjones@pmade.com>
2012-08-28 10:35:21 -0600

    Add a history-clear function to conkeror

--- conkeror/bindings.js
+++ conkeror/bindings.js
@@ -1,2 +1,11 @@
+function history_clear () {
+  var history = Cc["@mozilla.org/browser/nav-history-service;1"]
+    .getService(Ci.nsIBrowserHistory);
+  history.removeAllPages();
+}
+
+interactive("history-clear", "Clear the history.", history_clear);
+
+
 define_key(text_keymap, "C-w", "cmd_deleteWordBackward");
 define_key(content_buffer_normal_keymap, "c", "copy", $browser_object = browser_object_dom_node);
