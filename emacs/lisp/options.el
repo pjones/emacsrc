f9f7e34b91f1b9d42ff226c181f0e4acbcc6f74a
Peter Jones <pjones@pmade.com>
2012-11-07 16:08:29 -0700

    New function to change the default frame font for a projector

--- emacs/lisp/options.el
+++ emacs/lisp/options.el
@@ -1,4 +1,6 @@
 ;;; options.el -- Emacs settings not tied to any one mode.
+(eval-when-compile
+  (require 'server))
 
 ;; Personal information
 (setq user-full-name "Peter Jones"
@@ -42,6 +44,15 @@
       custom-file "~/.emacs.d/pjones/lisp/custom.el") ; To keep Emacs happy
 
 ;; Frame setup
+(defvar pjones:big-font
+  "-unknown-DejaVu Sans Mono-normal-normal-normal-*-22-*-*-*-m-0-iso10646-1"
+  "A large font to use for projectors.")
+
+(defun pjones:font-big ()
+  "Switch the current frame to a big font."
+  (interactive)
+  (set-frame-font pjones:big-font))
+
 (defun pjones:frame-title-file-name ()
   (let* ((home (expand-file-name "~"))
          (end (length home))
