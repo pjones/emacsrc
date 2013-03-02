bcfffa7215809371c42a022b2f91230a3bdb2e74
Peter Jones <pjones@pmade.com>
2013-03-02 15:33:51 -0700

    Emacs: more changes to make Haskell mode a bit better

--- modes/haskell-mode-conf.el
+++ modes/haskell-mode-conf.el
@@ -1,9 +1,18 @@
 ;;; haskell-mode-conf.el -- Settings for Haskell mode.
 (eval-when-compile
   (load "../lisp/code.el")
+  (require 'haskell-simple-indent)
   (require 'haskell-indent)
   (require 'haskell-mode))
 
-(setq haskell-indent-offset 2)
-(pjones:add-programming-hook 'haskell-mode-hook)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(defun pjones:haskell-mode-hook ()
+  (pjones:prog-mode-hook)
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (make-local-variable 'tab-always-indent)
+  (setq tab-always-indent t
+        haskell-indent-offset 4)
+  (turn-on-haskell-simple-indent))
+
+;;  (turn-on-haskell-indent)
+
+(add-hook 'haskell-mode-hook 'pjones:haskell-mode-hook)
