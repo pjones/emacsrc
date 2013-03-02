f2cf480c2e19e72447e599a44302166ddc1d1bc8
Peter Jones <pjones@pmade.com>
2013-03-02 15:36:15 -0700

    Emacs: Start writing a function to handle mailto links

--- emacs/modes/browse-url-conf.el
+++ emacs/modes/browse-url-conf.el
@@ -1,5 +1,23 @@
 ;;; browse-url-conf.el -- Settings for browse-url.
-(eval-when-compile (require 'browse-url))
+(eval-when-compile
+  (require 'gnus-msg)
+  (require 'message)
+  (require 'browse-url))
 
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "open-url")
+(declare-function message-goto-body "message")
+(declare-function gnus-group-mail "gnus-msg")
+
+(defun pjones:new-mail-in-gnus (url)
+  (let ((to (error "not implemented"))
+        (subject (error "not implemented")))
+    (gnus-group-mail)
+    (message-goto-to)
+    (insert to)
+    (unless (null subject)
+      (message-goto-subject)
+      (insert subject))
+    (message-goto-body)))
+
+(setq browse-url-generic-program "open-url"
+      browse-url-browser-function 'browse-url-generic
+      browse-url-mailto-function  'pjones:new-mail-in-gnus)
