0761932f147d5dbf9227ee66e107a03f54c61888
Peter Jones <pjones@pmade.com>
2013-04-10 17:34:31 -0600

    zsh: First major hacking on my prompt in like 10 years

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,25 +1,33 @@
 # Prompt magic
 autoload -U colors; colors
 
-if [[ "x$INSIDE_SCRIPT" = "x" ]]; then
-    setopt prompt_percent
-    setopt prompt_subst
-    prompt_newline=$'\n%{\r%}'
-    prompt_backtick='\`'
+if [ "x$INSIDE_SCRIPT" = "x" ]; then
+  setopt prompt_percent
+  setopt prompt_subst
+  prompt_newline=$'\n%{\r%}'
+  prompt_backtick='\`'
 
-    precmd () 
-    { 
-        lc=`fc -lnD -1|awk '{print $1}'`
-        branch=$(git_current_branch 2> /dev/null)
-        prompt="%{$fg[cyan]%}.----(%{$fg[blue]%} %n%{$fg[red]%}@%{$fg[green]%}%m %{$fg[cyan]%})--(%{$fg[magenta]%} %?/$lc %{$fg[cyan]%})--%{$fg[cyan]%}(%{$fg[yellow]%} %2~ %{$fg[cyan]%})"
-        
-        if [[ ${#branch} -ne 0 ]]; then
-          prompt="${prompt}--%{$fg[cyan]%}(%{$fg[red]%} $branch %{$fg[cyan]%})"
-        fi
+  precmd () {
+    # Begin prompt with user@host.
+    prompt="%F{cyan}.---(%F{blue}%n%F{red}@%F{green}%m%F{cyan})"
 
-        prompt="${prompt}${prompt_newline}${prompt_backtick}--%{$reset_color%}> "
-        PS1=$prompt
-    }
+    # Add conditional last process status
+    prompt="${prompt}%(?..--[%F{red}%B%?%b%F{cyan}])"
+
+    # Add the current directory two levels deep.
+    prompt="${prompt}--%F{cyan}(%F{yellow}%20<..<%2~%<<%F{cyan})"
+
+    # Maybe add in the current Git branch.
+    branch=$(git_current_branch 2> /dev/null)
+
+    if [[ ${#branch} -ne 0 ]]; then
+      prompt="${prompt}--%F{cyan}(%F{green}%12<..<$branch%<<%F{cyan})"
+    fi
+
+    # Move to the next line and present the command prompt.
+    prompt="${prompt}${prompt_newline}${prompt_backtick}-%F{white}>%f "
+    PS1=$prompt
+  }
 else
-    PS1="$ "
+  PS1="$ "
 fi
