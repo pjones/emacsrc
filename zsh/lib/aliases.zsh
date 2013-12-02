26b9c44680cfafaac506e1829751191c81595df3
Peter Jones <pjones@pmade.com>
2013-12-02 11:30:29 -0700

    zsh: New alias for terminal clock

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -55,3 +55,4 @@ dl () {dict "$@" | less}
 
 # Just to shorten typing
 alias tclock="tty-clock -c -C 4 -f '%b. %d, %Y'"
+alias scclock='env TZ=America/New_York tty-clock -C 0 -c -f "South Carolina"'
