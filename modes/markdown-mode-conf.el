0fb9c9a54f8cd15792008ba5b7be3e2312c4391e
Peter Jones <pjones@pmade.com>
2013-12-20 10:09:47 -0700

    Emacs: use orgmode tricks in markdown

--- modes/markdown-mode-conf.el
+++ modes/markdown-mode-conf.el
@@ -1,7 +1,13 @@
 ;;; markdown-conf.el -- Settings for markdown-mode.
 (eval-when-compile
-  (require 'markdown-mode))
+  (require 'markdown-mode)
+  (require 'org)
+  (require 'org-table))
 
+;; Silence a compiler warning.
+(declare-function orgtbl-mode "org-table")
+
+;; Basic settings.
 (setq markdown-command "pandoc -f markdown -t html"
       markdown-follow-wiki-link-on-enter nil)
 
@@ -9,6 +15,8 @@
   "Set up key bindings and other crap for markdown-mode."
   (local-set-key (kbd "C-c C-o") 'markdown-follow-link-at-point)
   (whitespace-mode)
+  (orgstruct-mode)
+  (orgtbl-mode)
 
   ;; Files in /tmp that are *.txt are from my browser and most
   ;; websites don't like it when text you submit has newlines.
