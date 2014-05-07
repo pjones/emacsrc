c57958f7fe411d4a278effdb528f00d8532ba0e5
Peter Jones <pjones@pmade.com>
2014-05-07 08:37:25 -0600

    Emacs, Gnus: Revert email address changes

--- lisp/options.el
+++ lisp/options.el
@@ -3,7 +3,7 @@
   (require 'server))
 
 ;; Personal information
-(setq user-full-name "Peter J. Jones"
+(setq user-full-name "Peter Jones"
       user-mail-address "pjones@devalot.com")
 
 ;; Things to turn on for all modes
