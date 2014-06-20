a3bda3299a4cf39e417780f98f9954146afe13a4
Peter Jones <pjones@pmade.com>
2014-06-20 08:28:23 -0600

    Finally add support for the rbenv Ruby tool

--- zsh/app/ruby.zsh
+++ zsh/app/ruby.zsh
@@ -1,2 +1,12 @@
+################################################################################
+# If the rbenv tool is installed, use it.
+if [ -d ~/.rbenv/bin ]; then
+  path=(~/.rbenv/bin $path)
+  eval "$(rbenv init -)"
+fi
+
+################################################################################
 alias irb='irb --readline -r irb/completion'
+
+################################################################################
 rtags () { command rtags --quiet $(find . -type f -name '*.rb')}
