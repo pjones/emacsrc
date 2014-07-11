0ab2a5354d74f65699162e9927a4cad55f45beae
Peter Jones <pjones@pmade.com>
2014-07-11 15:10:45 -0600

    Emacs: Pull out the ability to switch to visual line in markdown

--- emacs/modes/markdown-mode-conf.el
+++ emacs/modes/markdown-mode-conf.el
@@ -10,6 +10,13 @@
 ;; Basic settings.
 (setq markdown-command "pandoc -f markdown -t html")
 
+(defun pjones:markdown-visual-line ()
+  "Don't wrap lines.  Needed for most web forms."
+  (interactive)
+  (setq whitespace-style (delq 'lines-tail whitespace-style))
+  (auto-fill-mode -1)
+  (visual-line-mode))
+
 (defun pjones:markdown-mode-hook ()
   "Set up key bindings and other crap for markdown-mode."
   (local-set-key (kbd "C-c C-o") 'markdown-follow-link-at-point)
@@ -20,8 +27,6 @@
   ;; Files in /tmp that are *.txt are from my browser and most
   ;; websites don't like it when text you submit has newlines.
   (when (and buffer-file-name (string-match "^/tmp/.*\\.txt$" buffer-file-name))
-    (whitespace-mode -1)
-    (auto-fill-mode -1)
-    (visual-line-mode)))
+    (pjones:markdown-visual-line)))
 
 (add-hook 'markdown-mode-hook 'pjones:markdown-mode-hook)
