332a35b5c09677fd7093d9ed561ae4f054b4c9c8
Peter Jones <pjones@pmade.com>
2014-12-05 20:20:00 +0100

    Fix emacs, add ruby

--- nix/pkgs/base.nix
+++ nix/pkgs/base.nix
@@ -4,7 +4,14 @@
   # Packages needed on any system, including servers, workstations,
   # laptops, VMs, etc.
   packages = with pkgs; [
-    emacs tmux duplicity gnupg inotifyTools libxml2 libxslt pwgen
+    # Emacs, my best friend.  But it's better without GTK. (The Emacs
+    # daemon works better without GTK, that is.)
+    (emacs.override {withX = true; withGTK = false;})
+
+    # I still have a lot of scripts that require Ruby.
+    ruby_2_1
+
+    tmux duplicity gnupg inotifyTools libxml2 libxslt pwgen
     rsync unison zip unzip
   ];
 }
