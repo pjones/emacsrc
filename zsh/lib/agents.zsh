abbc3454097c093e6158917543dff858853e245e
Peter Jones <pjones@pmade.com>
2014-12-15 18:08:46 +0100

    Trying to get agent forwarding working correctly

--- zsh/lib/agents.zsh
+++ zsh/lib/agents.zsh
@@ -1,12 +1,18 @@
-# Load SSH agent environment vars.
-if [ -r ~/.ssh/agent ]; then
-  eval `cat ~/.ssh/agent | egrep -v ^echo`
-  export SSH_AGENT_PID
-  export SSH_AUTH_SOCK
-fi
+# Some variables to always export.
+export SSH_AGENT_PID
+export SSH_AUTH_SOCK
+export GPG_AGENT_INFO
 
-# Load GPG agent environment vars.
-if [ -r ~/.gpg-agent-info ]; then
-  eval `cat ~/.gpg-agent-info`
-  export GPG_AGENT_INFO
+# Only load these files in an GUI session
+if [ -n "$DISPLAY" ]; then
+
+  # Load SSH agent environment vars.
+  if [ -r ~/.ssh/agent ]; then
+      eval `cat ~/.ssh/agent | egrep -v ^echo`
+  fi
+
+  # Load GPG agent environment vars.
+  if [ -r ~/.gpg-agent-info ]; then
+      eval `cat ~/.gpg-agent-info`
+  fi
 fi
