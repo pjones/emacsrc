0ee30bdf64903333fb7908cc5398698d0489b7c9
Peter Jones <pjones@pmade.com>
2015-02-21 12:39:18 -0700

    Add the ability to control the scanned file name

--- bin/scan.sh
+++ bin/scan.sh
@@ -1,10 +1,36 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=~/.nix-profile/lib/sane
-export SANE_CONFIG_DIR=~/.nix-profile/etc/sane.d/
+################################################################################
+set -e
 
-mode=Color
+################################################################################
+if [ -d ~/.nix-profile/etc/sane.d/ ]; then
+  export LD_LIBRARY_PATH=~/.nix-profile/lib/sane
+  export SANE_CONFIG_DIR=~/.nix-profile/etc/sane.d/
+fi
+
+################################################################################
+mode=Gray # Color
 resolution=150
 size="-x 215.9 -y 279.4" # Letter
+base=`date +%Y-%m-%d_%H:%M:%S`
 
-scanimage --mode $mode --resolution $resolution $size > scan.pnm
+################################################################################
+if [ $# -gt 0 ]; then
+  base=$1
+  shift
+fi
+
+################################################################################
+if [ -r ${base}.pnm -o -r ${base}.pdf ]; then
+  echo "ERROR: $base basename is already taken!"
+  exit 1
+fi
+
+################################################################################
+scanimage --mode $mode --resolution $resolution $size > ${base}.pnm
+convert ${base}.pnm ${base}.pdf
+rm ${base}.pnm
+
+################################################################################
+echo "created ${base}.pdf"
