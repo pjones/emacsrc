9eab69a49f834301bc438bd154cb1193951fc6db
Peter Jones <pjones@pmade.com>
2015-11-15 10:51:32 -0700

    Proper configuration of Projectile

--- lisp/projects.el
+++ lisp/projects.el
@@ -6,17 +6,19 @@
 ;; Projectile is used to manage projects:
 ;; Need to fix the key binding before loading!
 (setq projectile-keymap-prefix (kbd "C-c C-p"))
+(require 'projectile)
+
+(defun pjones:projectile-mode-hook ()
+  (add-to-list 'projectile-project-root-files "GNUmakefile")
+  (add-to-list 'projectile-project-root-files "Rakefile")
+  (add-to-list 'projectile-project-root-files "package.json")
+  (add-to-list 'projectile-project-root-files-bottom-up ".dir-locals.el")
+  (setq projectile-switch-project-action 'projectile-dired))
+
+(add-to-list 'projectile-mode-hook 'pjones:projectile-mode-hook)
 
 (if (and (boundp 'server-name) (string= server-name "server"))
     (projectile-global-mode))
-
-;; Which files mark a project:
-(add-to-list 'projectile-project-root-files "GNUmakefile")
-(add-to-list 'projectile-project-root-files "Rakefile")
-(add-to-list 'projectile-project-root-files "package.json")
-(add-to-list 'projectile-project-root-files-bottom-up ".dir-locals.el")
-
-(setq projectile-switch-project-action 'projectile-dired)
 
 ;; Project settings (when I can't use a .dir-locals.el file):
 (dir-locals-set-class-variables 'cltc-new-project
@@ -25,10 +27,3 @@
 
 (dir-locals-set-directory-class
    (expand-file-name "~/develop/scors/cltc-claims") 'cltc-new-project)
-
-(dir-locals-set-class-variables 'xmonad
-  '((nil . ((projectile-project-compilation-cmd "cd xmonad && script/nix-env.sh")
-            (projectile-project-run-cmd "cd xmonad && script/install.sh restart")))))
-
-(dir-locals-set-directory-class
- (expand-file-name "~/core/xmonad") 'xmonad)
