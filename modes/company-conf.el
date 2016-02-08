3aa76ed907640ff35e0f843028107c5f6afa99f0
Peter Jones <pjones@pmade.com>
2016-02-08 15:13:16 -0700

    Fix C-w in company-active-map

--- modes/company-conf.el
+++ modes/company-conf.el
@@ -18,5 +18,12 @@
              (company-dabbrev-code company-gtags company-etags company-keywords)
              company-files company-dabbrev))))
 
-;; Enable fuzzy (flx) matching in company-mode.
-(company-flx-mode +1)
+(defun pjones:company-mode-hook ()
+  (let ((map company-active-map))
+    (define-key map (kbd "C-w") 'pjones:kill-region-or-backward-kill-word)
+    (define-key map (kbd "<tab>") 'company-complete-common-or-cycle))
+
+  ;; Enable fuzzy (flx) matching in company-mode.
+  (company-flx-mode +1))
+
+(add-hook 'company-mode-hook 'pjones:company-mode-hook)
