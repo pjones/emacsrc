d942099d3a9f9630ea50eeb8ad26c67480fc7a41
Peter Jones <pjones@devalot.com>
2017-02-01 21:49:41 -0700

    Comment out some older magit advice for now

--- modes/magit-conf.el
+++ modes/magit-conf.el
@@ -8,16 +8,16 @@
  '(magit-popup-use-prefix-argument 'default))
 
 ;; Shamelessly stolen from http://whattheemacsd.com/.
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun pjones:magit-quit-session ()
-  "Restore the previous window configuration and kill the magit
-buffer."
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
-(define-key magit-status-mode-map (kbd "q") 'pjones:magit-quit-session)
+;; (defadvice magit-status (around magit-fullscreen activate)
+;;   (window-configuration-to-register :magit-fullscreen)
+;;   ad-do-it
+;;   (delete-other-windows))
+;;
+;; (defun pjones:magit-quit-session ()
+;;   "Restore the previous window configuration and kill the magit
+;; buffer."
+;;   (interactive)
+;;   (kill-buffer)
+;;   (jump-to-register :magit-fullscreen))
+;;
+;; (define-key magit-status-mode-map (kbd "q") 'pjones:magit-quit-session)
