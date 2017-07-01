4f99d4da61cb00b9c4d00ef88356b2bf58a861a5
Peter Jones <pjones@devalot.com>
2017-06-30 17:13:12 -0700

    Hey Emacs C mode, I'm going to start using 2-space indents

--- modes/cc-mode-conf.el
+++ modes/cc-mode-conf.el
@@ -1,5 +1,5 @@
 ;;; cc-mode-conf.el -- Settings for c-mode
 (eval-when-compile (require 'cc-mode))
 
-(setq-default c-basic-offset 4
+(setq-default c-basic-offset 2
               c-default-style "bsd")
