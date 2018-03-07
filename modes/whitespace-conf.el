88d050e122df3e5aecad689e38ad8484297dd6a2
Peter Jones <pjones@devalot.com>
2018-03-06 23:27:34 -0700

    Don't show trailing whitespace via faces, it's just super distracting

--- modes/whitespace-conf.el
+++ modes/whitespace-conf.el
@@ -1,5 +1,5 @@
 ;;; whitespace.el -- Configuration options for whitespace-mode.
 (eval-when-compile (require 'whitespace))
 
-(setq whitespace-style '(face trailing tabs newline lines-tail empty)
+(setq whitespace-style '(face tabs newline lines-tail)
       whitespace-action '(auto-cleanup))
