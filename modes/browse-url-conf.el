2777dcd7de11e980d591965090ffd695db0cd25f
Peter Jones <pjones@devalot.com>
2018-05-01 12:48:25 -0700

    Updated browse-url to work with Chromium directly

--- modes/browse-url-conf.el
+++ modes/browse-url-conf.el
@@ -1,25 +1,11 @@
 ;;; browse-url-conf.el -- Settings for browse-url.
 (eval-when-compile
-  (require 'gnus-msg)
-  (require 'message)
   (require 'browse-url))
 
-(declare-function message-goto-body "message")
-(declare-function message-goto-to "message")
-(declare-function message-goto-subject "message")
-(declare-function gnus-group-mail "gnus-msg")
+(defun pjones:browse-url (url &optional _new-window)
+  "Open a URL in a external browser."
+  (interactive)
+  (call-process "bspc" nil 0 nil "desktop" "browsers" "--focus")
+  (call-process "chromium" nil 0 nil url))
 
-(defun pjones:new-mail-in-gnus (url)
-  (let ((to (error "not implemented"))
-        (subject (error "not implemented")))
-    (gnus-group-mail)
-    (message-goto-to)
-    (insert to)
-    (unless (null subject)
-      (message-goto-subject)
-      (insert subject))
-    (message-goto-body)))
-
-(setq browse-url-generic-program "open-url"
-      browse-url-browser-function 'browse-url-generic
-      browse-url-mailto-function  'pjones:new-mail-in-gnus)
+(setq browse-url-browser-function #'pjones:browse-url)
