df574fa0d6f97e654ac30d1b08fc6b87e51cce46
Peter Jones <pjones@devalot.com>
2018-05-01 12:49:03 -0700

    Force ido to always display a buffer in the current window

--- modes/ido-conf.el
+++ modes/ido-conf.el
@@ -6,6 +6,8 @@
 (custom-set-variables
   '(ido-auto-merge-work-directories-length nil)
   '(ido-create-new-buffer 'always)
+  '(ido-default-buffer-method 'selected-window)
+  '(ido-default-file-method 'selected-window)
   '(ido-enable-flex-matching t)
   '(ido-enable-prefix nil)
   '(ido-max-prospects 10)
