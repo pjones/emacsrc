6489e2c09951af59f211ccfe7ea74e27c7ea1c91
Peter Jones <pjones@devalot.com>
2018-05-12 21:50:56 -0700

    Add some EXWM settings that require a restart

--- modes/exwm-conf.el
+++ modes/exwm-conf.el
@@ -4,14 +4,25 @@
 (eval-when-compile
   (require 'exwm))
 
+;; Load optional EXWM features:
+(require 'exwm-randr)
+(require 'exwm-systemtray)
+
 (defun pjones:exwm-update-class-hook ()
   "Update shit after a class name changes."
   (exwm-workspace-rename-buffer exwm-class-name))
 
 (custom-set-variables
+   ;; Move minibuffer to the top of the screen and auto hide:
+   '(exwm-workspace-minibuffer-position 'top)
+
+   ;; Number of workspaces:
   '(exwm-workspace-number 9)
 
-  ;; Simulated key presses to X Windows.
+  ;; RandR settings:
+  '(exwm-randr-workspace-output-plist '(0 "eDP1"))
+
+  ;; Simulated key presses to X Windows:
   '(exwm-input-simulation-keys
     '(([?\C-b] . [left])
       ([?\C-f] . [right])
@@ -24,6 +35,12 @@
       ([?\C-d] . [delete])
       ([?\C-k] . [S-end delete]))))
 
-(exwm-input-set-key (kbd "s-r")   #'exwm-reset)
-(exwm-input-set-key (kbd "s-w")   #'exwm-workspace-switch)
+(exwm-input-set-key (kbd "s-r") #'exwm-reset)
+(exwm-input-set-key (kbd "s-w") #'exwm-workspace-switch)
+
+;; Activate optional features:
+(exwm-randr-enable)
+(exwm-systemtray-enable)
+
+;; Insert some hooks:
 (add-hook 'exwm-update-class-hook #'pjones:exwm-update-class-hook)
