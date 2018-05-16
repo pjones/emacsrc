c25a6f9320131073f4f035a492463892240279e1
Peter Jones <pjones@devalot.com>
2018-05-16 09:35:38 -0700

    Removed some old ido settings from magit-conf.el

--- modes/magit-conf.el
+++ modes/magit-conf.el
@@ -7,7 +7,4 @@
 (custom-set-variables
  '(magit-popup-use-prefix-argument 'default)
  '(magit-status-margin '(t age magit-log-margin-width nil 18))
- '(magit-status-show-hashes-in-headers t)
- '(magit-restore-window-configuration t)
- '(magit-completing-read-function (quote magit-ido-completing-read))
- '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-topleft-v1)))
+ '(magit-status-show-hashes-in-headers t))
