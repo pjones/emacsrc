134bd3e72e271656ddbf899a5f5a109524bf778a
Peter Jones <pjones@devalot.com>
2018-06-27 11:52:48 -0700

    helm: playing with some settings

--- modes/helm-conf.el
+++ modes/helm-conf.el
@@ -6,12 +6,13 @@
 (eval-when-compile
   (require 'helm))
 
+;; Settings:
 (custom-set-variables
- '(helm-echo-input-in-header-line t)
+ '(helm-ff-fuzzy-matching nil)
+ '(helm-echo-input-in-header-line nil)
  '(helm-follow-mode-persistent t)
+ '(helm-source-names-using-follow '("Buffers"))
+ '(helm-show-action-window-other-window 'right)
  `(helm-candidate-separator ,(make-string 78 ?─)))
-
-;;; Hooks:
-(add-hook 'helm-minibuffer-set-up-hook #'helm-hide-minibuffer-maybe)
 
 ;;; helm-conf.el ends here
