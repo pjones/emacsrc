ae7958871cde4feb0ceb8bb649524cc485fcf3ed
Peter Jones <pjones@devalot.com>
2018-07-06 13:15:40 -0700

    modes: add configuration for desktop-save-mode

---
+++ modes/desktop-conf.el
@@ -0,0 +1,15 @@
+;;; desktop-conf.el --- Settings for `desktop-save-mode'
+
+;;; Commentary:
+;;
+;; Settings for `desktop-save-mode'.
+
+;;; Code:
+(eval-when-compile
+  (require 'desktop))
+
+(custom-set-variables
+ '(desktop-save t)
+ '(desktop-load-locked-desktop t))
+
+;;; desktop-conf.el ends here
