8b3aae554d7a0874b78211fda17ae038068b6939
Peter Jones <pjones@devalot.com>
2018-08-08 09:06:09 -0700

    modes: add settings for TRAMP

---
+++ modes/tramp-conf.el
@@ -0,0 +1,10 @@
+;;; tramp-conf.el --- Settings for TRAMP.
+;;
+;;; Commentary:
+;;; Code:
+
+(custom-set-variables
+ '(tramp-terminal-type "dumb")
+ '(tramp-default-method "ssh"))
+
+;;; tramp-conf.el ends here
