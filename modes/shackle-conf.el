0802d7609abab692a672e105a201e7627ccb26d6
Peter Jones <pjones@devalot.com>
2018-11-03 16:24:08 -0700

    shackle: improve magit rules, fix broken completions rules

--- modes/shackle-conf.el
+++ modes/shackle-conf.el
@@ -18,14 +18,15 @@
 (add-to-list 'shackle-rules '("*JS scratch*" :regex t :same nil))
 
 ;; Modes that should get their own windows, but remain inactive:
-(add-to-list 'shackle-rules '(compilation-mode :same nil :noselect t))
-(add-to-list 'shackle-rules '(grep-mode :same nil :noselect t))
-(add-to-list 'shackle-rules '(magit-diff-mode :same nil :noselect t))
+(add-to-list 'shackle-rules '(compilation-mode :noselect t :same nil))
+(add-to-list 'shackle-rules '(grep-mode :noselect t :same nil))
+(add-to-list 'shackle-rules '(magit-diff-mode :noselect t :same nil))
 
 ;; Windows that should split the entire frame:
-(add-to-list 'shackle-rules '("*Completions*" :same nil :size 0.3 :align 'below))
-(add-to-list 'shackle-rules '(calendar-mode :same nil :size 0.2 :align 'below))
-(add-to-list 'shackle-rules '("\\*magit-.*popup" :regexp t :align 'below))
+(add-to-list 'shackle-rules '("*Completions*" :regexp t :size 0.3 :align 'below :noselect t))
+(add-to-list 'shackle-rules '("*Deletions*" :regexp t :size 0.3 :align 'below :noselect t))
+(add-to-list 'shackle-rules '(calendar-mode :size 0.2 :align 'below :noselect t))
+(add-to-list 'shackle-rules '("\\*magit-.*popup" :regexp t :custom pjones:shackle-split))
 
 ;; PDF Outline windows should always split the current window:
 (add-to-list 'shackle-rules
