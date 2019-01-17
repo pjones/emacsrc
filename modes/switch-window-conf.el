3e017598ba6762e7d27e9cbc9f47a28f8dec53b7
Peter Jones <pjones@devalot.com>
2019-01-16 17:11:07 -0700

    switch-window: Update settings to better support EXWM

--- modes/switch-window-conf.el
+++ modes/switch-window-conf.el
@@ -1,13 +1,17 @@
 ;;; switch-window-conf.el -- Settings for switch-window.
-(eval-when-compile
-  (require 'switch-window))
+;;
+;;; Commentary:
+;;
+;;; Code:
+(require 'switch-window)
 
 (custom-set-variables
  '(switch-window-shortcut-style 'qwerty)
- '(switch-window-increase 8)
+ '(switch-window-minibuffer-shortcut ?z)
  '(switch-window-timeout 5)
  '(switch-window-threshold 2)
  '(switch-window-preferred 'ivy)
- '(switch-window-configuration-change-hook-inhibit t))
+ '(switch-window-configuration-change-hook-inhibit t)
+ '(switch-window-input-style 'minibuffer))
 
 ;;; switch-window-conf.el ends here
