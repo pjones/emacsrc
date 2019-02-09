6a89f8b37d6c689d0cf0a3eb02128e859f45cf99
Peter Jones <pjones@devalot.com>
2019-02-08 17:07:45 -0700

    modes: Only load the mode -conf files once
    Previously the code was loading *.el and then *.elc.  Yuck.

--- lisp/modes.el
+++ lisp/modes.el
@@ -9,8 +9,8 @@
 
 ;; Automatically load my per-mode configuration files
 (dolist (file (directory-files pjones:modes-dir t))
-  (let ((basename (file-name-sans-extension (file-name-nondirectory file))))
-    (when (string-match "\\(-conf\\)$" basename)
+  (let ((basename (file-name-nondirectory file)))
+    (when (string-match "\\(-conf\\.elc\\)$" basename)
       (eval-after-load (intern (replace-match "" t t basename))
         `(load ,file)))))
 
