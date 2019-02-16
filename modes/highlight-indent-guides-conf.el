02e98383c6b5e58236fc5a70396dffcf534b7e8c
Peter Jones <pjones@devalot.com>
2019-02-16 13:33:31 -0700

    pkgs: Remove unused mode config

--- modes/highlight-indent-guides-conf.el
+++
@@ -1,8 +0,0 @@
-;;; highlight-indent-guides-conf.el -- Settings for highlight-indent-guides.
-(eval-when-compile
-  (require 'highlight-indent-guides))
-
-(custom-set-variables
- '(highlight-indent-guides-method 'character))
-
-(provide 'highlight-indent-guides-conf)
