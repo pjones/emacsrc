55e1318c544bf98e46dafa7a476ac1c67668312b
Peter Jones <pjones@devalot.com>
2019-05-16 11:18:40 -0700

    flycheck + haskell: Don't half-save files and break my buffers

--- modes/flycheck-conf.el
+++ modes/flycheck-conf.el
@@ -1,7 +1,15 @@
 ;;; flycheck-conf.el -- Configuration for flycheck.
-(eval-when-compile
-  (require 'flycheck))
+;;
+;;; Commentary:
+;;
+;;; Code:
+(require 'flycheck)
 
 (custom-set-variables
  '(flycheck-disabled-checkers '(javascript-eslint javascript-gjslint))
- '(flycheck-standard-error-navigation nil))
+ '(flycheck-standard-error-navigation nil)
+ '(flymake-no-changes-timeout nil)
+ '(flymake-start-syntax-check-on-newline nil)
+ '(flycheck-check-syntax-automatically '(save mode-enabled)))
+
+;;; flycheck-conf.el ends here
