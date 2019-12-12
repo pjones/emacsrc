611a50956e280912f02f4fba11ea9f62fd3ba633
Peter Jones <pjones@devalot.com>
2019-12-12 09:22:00 -0700

    Switch font to Fira Code

--- lisp/options.el
+++ lisp/options.el
@@ -88,7 +88,7 @@
   (set-frame-parameter frame 'name  nil))
 
 (add-to-list 'default-frame-alist '(cursor-type  . hbar))
-(add-to-list 'default-frame-alist '(font . "Office Code Pro:pixelsize=12"))
+(add-to-list 'default-frame-alist '(font . "Fira Code:pixelsize=14"))
 (setq frame-title-format '(:eval (pjones:frame-title-file-name)))
 
 (defun pjones:find-file-hook ()
