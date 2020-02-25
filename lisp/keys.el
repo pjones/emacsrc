14d66ab111de51475a040adfbab01f7a0fca5691
Peter Jones <pjones@devalot.com>
2020-02-24 17:41:21 -0700

    keys: Add binding for deleting blank lines around point

--- lisp/keys.el
+++ lisp/keys.el
@@ -114,6 +114,7 @@
   "j"     #'erc-track-switch-buffer
   "m"     #'magit-status
   "n"     #'flycheck-next-error
+  "o"     #'delete-blank-lines
   "p f"   #'pjones:projectile-find-file
   "q"     #'kill-this-buffer
   "s"     #'pjones:evil-sort
