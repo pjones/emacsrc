f4df524c59da8469667d50d212a7eabaadcc57c7
Peter Jones <pjones@devalot.com>
2020-05-14 15:17:01 -0700

    eglot: Restore default behavior for the echo area

--- modes/eglot-conf.el
+++ modes/eglot-conf.el
@@ -7,7 +7,6 @@
 
 (custom-set-variables
  '(eglot-auto-display-help-buffer t)
- '(eglot-autoshutdown t)
- '(eglot-put-doc-in-help-buffer t))
+ '(eglot-autoshutdown t))
 
 ;;; eglot-conf.el ends here
