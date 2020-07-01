87e583c9d36ce1e2a40727c20824632f4ae527f2
Peter Jones <pjones@devalot.com>
2020-07-01 11:05:08 -0700

    Make pandoc happy by setting a title when previewing documents

--- modes/markdown-mode-conf.el
+++ modes/markdown-mode-conf.el
@@ -21,7 +21,7 @@
  '(markdown-reference-location 'end)
  '(markdown-asymmetric-header t)
  '(markdown-hide-urls t)
- '(markdown-command "pandoc --standalone -f markdown -t html"))
+ '(markdown-command "pandoc --standalone -M title=Preview -f markdown -t html"))
 
 (custom-set-faces
  '(markdown-code-face ((t (:background nil))))
