71748e8ac4f8bc7bb9f179a28d366d16d2672c8e
Peter Jones <pjones@devalot.com>
2020-10-21 09:53:39 -0700

    Add new `editor' script, fix --no-wait, remove unused scripts

--- bin/wait-emacs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script is needed when some stupid software (Firefox) will open
-# execute commands without arguments.  It invokes emacsclient with
-# flags to open a new frame, and wait for the file to be done being
-# edited.
-exec e -c -w -- "$@"
