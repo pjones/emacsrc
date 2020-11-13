15af8bfd34b6094152cee637d8ac5ecc4af4f827
Peter Jones <pjones@devalot.com>
2020-11-13 11:34:37 -0700

    markdown: Fix markdown previews

--- modes/markdown-mode-conf.el
+++ modes/markdown-mode-conf.el
@@ -21,7 +21,7 @@
  '(markdown-reference-location 'end)
  '(markdown-asymmetric-header t)
  '(markdown-hide-urls t)
- '(markdown-command "pandoc --standalone -T Preview -f markdown -t html 2> /dev/null"))
+ '(markdown-command "pandoc"))
 
 (custom-set-faces
  '(markdown-code-face ((t (:background nil))))
