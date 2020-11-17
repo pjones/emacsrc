f01f4d45dd48d70344b408aafec8038625bf8377
Peter Jones <pjones@devalot.com>
2020-11-17 16:10:57 -0700

    magit: Populate `magit-repository-directories' on first load

--- modes/magit-conf.el
+++ modes/magit-conf.el
@@ -18,13 +18,24 @@
          (buffer-file-name)))
     "../lisp/macros")))
 
+(defun pjones:magit-repository-directories ()
+  "Generate a directory list for `magit-list-repositories'."
+  (let ((base (expand-file-name "~/src"))
+        (ls (lambda (dir) (directory-files dir t "^[^.]"))))
+    (-map (lambda (dir) (cons dir 0))
+          (-filter
+           (lambda (dir) (file-directory-p (concat dir "/.git/")))
+           (-flatten (-map ls (funcall ls base)))))))
+
 (custom-set-variables
  '(magit-completing-read-function #'ivy-completing-read)
  '(magit-popup-use-prefix-argument 'default)
  '(magit-status-margin '(t age magit-log-margin-width nil 18))
  '(magit-status-show-hashes-in-headers t)
  '(magit-section-initial-visibility-alist
-   '(([unpushed status] . show))))
+   '(([unpushed status] . show)))
+ '(magit-repository-directories
+   (pjones:magit-repository-directories)))
 
 (pjones:evil-override-mode magit-mode)
 
