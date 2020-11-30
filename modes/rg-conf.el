078c267f423ecce2b42968281088d843d2ca2063
Peter Jones <pjones@devalot.com>
2020-11-30 14:19:02 -0700

    Emulate a missing feature: `compilation-auto-jump-to-first-error'

--- modes/rg-conf.el
+++ modes/rg-conf.el
@@ -1,8 +1,10 @@
-;;; rg-conf.el -- Settings for `rg'
+;;; rg-conf.el -- Settings for `rg' -*- lexical-binding: t -*-
 ;;
 ;;; Commentary:
 ;;
 ;;; Code:
+
+(require 'compile)
 (require 'evil)
 (require 'rg)
 
@@ -16,5 +18,14 @@
     "../lisp/macros")))
 
 (pjones:evil-override-mode rg-mode)
+
+(defun pjones:rg-compilation-auto-jump ()
+  "Emulate `compilation-auto-jump-to-first-error'."
+  (when (and (> rg-hit-count 0) compilation-auto-jump-to-first-error)
+    (goto-char (point-min))
+    (compilation-next-error 1)
+    (compile-goto-error)))
+
+(add-hook 'rg-filter-hook #'pjones:rg-compilation-auto-jump)
 
 ;;; rg-conf.el ends here
