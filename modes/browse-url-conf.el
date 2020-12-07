208a7fec406a561ca98d8a07ae50a35abb300eda
Peter Jones <pjones@devalot.com>
2020-12-07 14:48:26 -0700

    browse-url: Dispatch URLs to the most appropriate browser window

--- modes/browse-url-conf.el
+++ modes/browse-url-conf.el
@@ -1,16 +1,22 @@
-;;; browse-url-conf.el -- Settings for browse-url.
+;;; browse-url-conf.el -- Settings for browse-url.  -*- lexical-binding: t -*-
 ;;
 ;;; Commentary:
 ;;
 ;;; Code:
 ;; Dependencies.
+
 (require 'async)
 (require 'browse-url)
+(require 's)
 
 (defun pjones:browse-url-browser-function (url &rest _args)
   "Open URL in a external browser."
   (interactive)
-  (async-start-process "chromium" "chromium" nil url))
+  (let ((prog
+         (if (s-prefix-p "file:" url)
+             "browser-sidebar"
+           "browser-main")))
+    (async-start-process prog prog nil url)))
 
 (setq browse-url-browser-function #'pjones:browse-url-browser-function)
 
