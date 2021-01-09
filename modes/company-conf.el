f8f7101c628d1a308d1287b8c8893ee802e91d66
Peter Jones <pjones@devalot.com>
2021-01-09 12:41:29 -0700

    company: One big backend that merges the only completions I care about

--- modes/company-conf.el
+++ modes/company-conf.el
@@ -18,34 +18,36 @@
   '(company-dabbrev-code-other-buffers 'all)
   '(company-tooltip-align-annotations t)
   '(company-quickhelp-use-propertized-text t)
-  '(company-transformers '(company-sort-by-occurrence))
-  '(company-frontends '(company-pseudo-tooltip-frontend
-                        company-preview-frontend
-                        company-echo-metadata-frontend))
-  '(company-backends '(company-capf
-                       company-files
-                       (company-dabbrev-code
-                        company-etags
-                        company-keywords)
-                       company-dabbrev)))
+  '(company-transformers
+    '(company-sort-by-occurrence))
+  '(company-frontends
+    '(company-pseudo-tooltip-frontend
+      company-preview-frontend
+      company-echo-metadata-frontend))
+  '(company-backends
+     '((company-capf
+        company-dabbrev-code
+        company-keywords
+        company-files))))
 
-(defun pjones:company-mode-hook ()
-  "Peter's mode hook for company-mode."
-  (let ((map company-active-map))
-    (define-key map (kbd "<escape>") #'company-abort)
-    (define-key map (kbd "C-f") #'company-abort)
-    (define-key map (kbd "C-/") #'company-filter-candidates)
-    (define-key map (kbd "C-h") #'company-quickhelp-manual-begin)
-    (define-key map (kbd "<tab>") #'company-complete-common-or-cycle))
+(let ((map company-active-map))
+  (define-key map (kbd "<escape>") #'company-abort)
+  (define-key map (kbd "C-f") #'company-abort)
+  (define-key map (kbd "C-/") #'company-filter-candidates)
+  (define-key map (kbd "C-h") #'company-quickhelp-manual-begin)
+  (define-key map (kbd "<tab>") #'company-complete-common-or-cycle)
+  (dotimes (i 10)
+    (define-key
+      map
+      (read-kbd-macro (format "C-%d" i))
+      'company-complete-number)))
 
-  (let ((map company-search-map))
-    (define-key map (kbd "<escape>") #'company-abort)
-    (define-key map (kbd "C-f") #'company-abort)
-    (define-key map (kbd "n") #'company-search-repeat-forward))
+(let ((map company-search-map))
+  (define-key map (kbd "<escape>") #'company-abort)
+  (define-key map (kbd "C-f") #'company-abort)
+  (define-key map (kbd "n") #'company-search-repeat-forward))
 
-  (company-quickhelp-mode)
-  (company-statistics-mode))
-
-(add-hook 'company-mode-hook 'pjones:company-mode-hook)
+(add-hook 'company-mode-hook #'company-quickhelp-mode)
+(add-hook 'company-mode-hook #'company-statistics-mode)
 
 ;;; company-conf.el ends here
