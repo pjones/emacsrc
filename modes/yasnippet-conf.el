ac6e492f5489e27fefac401bf2e311ae71b73cd7
Peter Jones <pjones@devalot.com>
2021-03-01 09:59:32 -0700

    Incremental improvement to snippets

--- modes/yasnippet-conf.el
+++ modes/yasnippet-conf.el
@@ -12,6 +12,14 @@
      (file-name-directory load-file-name))) "snippets/")
   "The directory where I keep snippet files.")
 
+(defun pjones:yas-bolp nil
+  "Return non-nil if snippet would expand at `bolp`."
+  (let ((begin (save-excursion
+                 (beginning-of-line)
+                 (point)))
+        (end (point)))
+    (not (string-match-p "\\s-" (buffer-substring begin end)))))
+
 ;; I already have a key that triggers yas-expand.
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 
