ce11b298b80d6b48908a2511122de474182429c1
Peter Jones <pjones@devalot.com>
2021-03-29 08:42:13 -0700

    whitespace: Better support for visual-line-mode

--- modes/whitespace-conf.el
+++ modes/whitespace-conf.el
@@ -3,10 +3,33 @@
 ;;; Commentary:
 ;;
 ;;; Code:
+
+(require 'dash)
+(require 'simple)
 (require 'whitespace)
 
+(defvar pjones:whitespace-style
+  '(face tabs newline lines-tail trailing)
+  "Default value for `whitespace-style'.")
+
 (custom-set-variables
- '(whitespace-style '(face tabs newline lines-tail trailing))
+ '(whitespace-style pjones:whitespace-style)
  '(whitespace-action '(auto-cleanup)))
+
+(defun pjones:whitespace-and-visual-line ()
+  "Alter `whitespace-mode' when variable `visual-line-mode' is non-nil."
+  (when whitespace-mode
+    (setq-local whitespace-style
+                (if visual-line-mode
+                    (-reject
+                     (apply-partially 'equal 'lines-tail)
+                     pjones:whitespace-style)
+                  pjones:whitespace-style))
+    ;; I can't figure out how to force `whitespace-mode' to update
+    ;; it's faces so I just toggle it.
+    (whitespace-mode -1)
+    (whitespace-mode +1)))
+
+(add-hook 'visual-line-mode-hook #'pjones:whitespace-and-visual-line)
 
 ;;; whitespace-conf.el ends here
