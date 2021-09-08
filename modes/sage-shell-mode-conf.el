d46981327a0623aeec89a39a1a690d79ca164851
Peter Jones <pjones@devalot.com>
2021-09-08 16:03:23 -0700

    sage: Add some useful settings for sage

---
+++ modes/sage-shell-mode-conf.el
@@ -0,0 +1,16 @@
+;;; sage-shell-mode-conf.el -- Settings for `sage-shell-mode' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'sage-shell-mode)
+
+(custom-set-variables
+ '(sage-shell:scroll-to-the-bottom t)
+ '(sage-shell:check-ipython-version-on-startup nil)
+ '(sage-shell:set-ipython-version-on-startup nil)
+ '(sage-shell:use-unicode-banner nil)
+ '(sage-shell:ask-command-options nil))
+
+;;; sage-shell-mode-conf.el ends here
