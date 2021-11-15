00203e5797b57b1285a03b42af6c8e555d3c7d4a
Peter Jones <pjones@devalot.com>
2021-11-15 15:58:13 -0700

    python: Remove some annoying symbol replacements

--- modes/python-conf.el
+++ modes/python-conf.el
@@ -15,6 +15,15 @@
   :args '("-")
   :group 'python)
 
+(defun pjones:python-mode-hook ()
+  "Hook function for `python-mode'."
+  (setq prettify-symbols-alist
+        (assoc-delete-all "and"
+         (assoc-delete-all "or"
+           prettify-symbols-alist))))
+
 (add-hook 'python-mode-hook #'python-format-on-save-mode)
+(add-hook 'python-mode-hook #'pjones:python-mode-hook)
+(add-hook 'inferior-python-mode-hook #'pjones:python-mode-hook)
 
 ;;; python-conf.el ends here
