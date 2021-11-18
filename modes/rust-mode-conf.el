7303cdef1e27dcd77310b028b691a0229a671054
Peter Jones <pjones@devalot.com>
2021-11-18 15:29:20 -0700

    rust: Disable some annoying rust-mode features

--- modes/rust-mode-conf.el
+++ modes/rust-mode-conf.el
@@ -7,6 +7,8 @@
 (require 'rust-mode)
 
 (custom-set-variables
-  '(rust-format-on-save t))
+ '(rust-format-on-save t)
+ '(rust-format-show-buffer nil)
+ '(rust-format-goto-problem nil))
 
 ;;; rust-mode-conf.el ends here
