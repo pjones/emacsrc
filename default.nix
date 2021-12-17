bf4237687357e2cb71c67865b6504512cd3ce7fd
Peter Jones <pjones@devalot.com>
2021-12-17 16:27:27 -0700

    pkgs: Push some packages out to my tilde repo

--- default.nix
+++ default.nix
@@ -25,26 +25,14 @@ pkgs.stdenv.mkDerivation rec {
 
   buildInputs = [
     emacsAndPackages # Emacs!
-  ] ++ lib.optionals (builtins.elem pkgs.system lib.platforms.x86) [
-    # Tools for desktop systems:
-    pkgs.black # For formatting Python code
-    pkgs.gitAndTools.gitFull # Needed to compile magit config
-    pkgs.gnuplot # For org-mode
-    pkgs.imagemagick # For image-mode and eimp-mode
+    pkgs.imagemagick # For image-mode
+  ] ++ lib.optionals pkgs.stdenv.isLinux [
     pkgs.netcat # For bin/e
-    pkgs.neuron-notes # For neuron-mode
-    pkgs.nixpkgs-fmt # Formatting nix files
-    pkgs.shellcheck # Lint shell scripts
-    pkgs.shfmt # Format shell scripts
-  ] ++ lib.optionals (builtins.elem pkgs.system lib.platforms.linux) [
-    # Tools that only work on Linux:
     pkgs.wmctrl # For bin/e
-  ] ++ lib.optionals (pkgs.system == "x86_64-linux") [
-    # Broken except for Linux on x86:
-    pkgs.sage # for sage-shell-mode
   ];
 
-  propagatedUserEnvPkgs = buildInputs;
+  # Ensure 'Emacs' makes it into the closure:
+  propagatedUserEnvPkgs = [ emacsAndPackages ];
 
   postInstall = with pkgs.lib;
     let path = makeBinPath buildInputs; in
