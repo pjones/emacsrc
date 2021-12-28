fddee40fc1a476b7079a8cbc449a6c4f43ffca86
Peter Jones <pjones@devalot.com>
2021-12-28 11:37:54 -0700

    elisp: Use the current load-path when running flycheck
    This lets flycheck see the packages that are loaded into the current
    Emacs session and prevents it from stopping at `require` lines due to
    errors.

--- modes/flycheck-conf.el
+++ modes/flycheck-conf.el
@@ -7,6 +7,7 @@
 
 (custom-set-variables
  '(flycheck-disabled-checkers '(javascript-gjslint))
+ '(flycheck-emacs-lisp-load-path 'inherit)
  '(flycheck-standard-error-navigation nil))
 
 ;;; flycheck-conf.el ends here
