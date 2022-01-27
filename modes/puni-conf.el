450bb540fc196acd6be1c2cf9ed5aaab03855c90
Peter Jones <pjones@devalot.com>
2022-01-27 12:12:41 -0700

    puni: Allow me to break syntax, expose more key bindings

--- modes/puni-conf.el
+++ modes/puni-conf.el
@@ -6,7 +6,21 @@
 
 (require 'puni)
 
-(let ((mode puni-mode-map))
-  (define-key mode (kbd "C-w") nil))
+(defun pjones:puni-squeeze-splice (arg)
+  "Perform a squeeze.
+If ARG is non-nil then perform a splice."
+  (interactive "P")
+  (if arg (puni-splice)
+    (puni-squeeze)))
+
+(let ((map puni-mode-map))
+  ;; I can't deal with my editor preventing me from deleting
+  ;; delimiters:
+  (define-key map (kbd "DEL") nil)
+  (define-key map (kbd "C-d") nil)
+  (define-key map (kbd "C-w") nil)
+  (define-key map (kbd "C-M-/") #'puni-split)
+  (define-key map (kbd "C-M-k") #'pjones:puni-squeeze-splice)
+  (define-key map (kbd "C-M-t") #'puni-transpose))
 
 ;;; puni-conf.el ends here
