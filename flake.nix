53d839df3ba1c8cb41d2941e9883af75290507fe
Peter Jones <pjones@devalot.com>
2022-04-24 13:17:27 -0700

    NixOS 22.05: Update to the nixos-unstable branch

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     cape = { url = "github:minad/cape/0.5"; flake = false; };
