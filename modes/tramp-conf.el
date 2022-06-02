92f27f7b72951e34fcc4fed2f19465174038ef54
Peter Jones <pjones@devalot.com>
2022-06-02 11:18:40 -0700

    tramp: Help tramp find executables on NixOS machines

--- modes/tramp-conf.el
+++ modes/tramp-conf.el
@@ -3,8 +3,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'tramp)
+
 (custom-set-variables
  '(tramp-terminal-type "dumb")
  '(tramp-default-method "ssh"))
+
+(add-to-list
+ 'tramp-remote-path
+ (concat "/etc/profiles/per-user/" user-login-name "/bin"))
 
 ;;; tramp-conf.el ends here
