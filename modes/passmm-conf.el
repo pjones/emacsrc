9380bca0672a76e3828c2ca6e2f9621b8ee392d7
Peter Jones <pjones@devalot.com>
2022-12-04 12:35:06 -0700

    passmm: Update to the latest release

---
+++ modes/passmm-conf.el
@@ -0,0 +1,12 @@
+;;; passmm-conf.el -- Settings for `passmm' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'passmm)
+
+(custom-set-variables
+  '(passmm-prompt-action 'open))
+
+;;; passmm-conf.el ends here
