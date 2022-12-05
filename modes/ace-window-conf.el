4ea5ef7d8b0f9c1a312988c47e91c701f2485fc7
Peter Jones <pjones@devalot.com>
2022-12-05 08:29:37 -0700

    ace-window: Improve visibility of window indicator

--- modes/ace-window-conf.el
+++ modes/ace-window-conf.el
@@ -10,4 +10,9 @@
  '(aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
  '(aw-scope 'frame))
 
+(custom-set-faces
+ '(aw-leading-char-face ((t (:inherit avy-lead-face
+                             :weight bold
+                             :height 2.0)))))
+
 ;;; ace-window-conf.el ends here
