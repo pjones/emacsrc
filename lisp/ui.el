4b22d0b6fee8a24e034a67017af9ac3c766b0a0b
Peter Jones <pjones@devalot.com>
2023-01-17 11:47:26 -0700

    theme: Change the default theme to match GTK

--- lisp/ui.el
+++ lisp/ui.el
@@ -200,7 +200,7 @@ The mode construct list is then returned."
 ;; Hooks:
 (add-hook 'after-init-hook
   (defun pjones:set-initial-theme ()
-    (pjones:load-theme 'doom-tokyo-night)))
+    (pjones:load-theme 'doom-dracula)))
 
 (add-hook 'after-init-hook #'pjones:configure-new-frame)
 (add-hook 'after-make-frame-functions #'pjones:configure-new-frame)
