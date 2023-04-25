fa5922748e09ec1c9d5357f9f34384ed09d2953d
Peter Jones <pjones@devalot.com>
2023-04-25 09:04:21 -0700

    project: Remove switch project hack, it stopped working
    My hack to use a better interface for project switching stopped
    working for some reason.  Ditch it for the default system which works
    but is a little clunky.

--- modes/project-conf.el
+++ modes/project-conf.el
@@ -11,13 +11,9 @@
 (declare-function pjones:projectile-compile-project "./projectile-conf")
 
 (declare-function magit-project-status "magit-extras")
+(declare-function magit-file-dispatch "magit-files")
 (declare-function pjones:keymap-popup-show "../lisp/interactive")
 (declare-function vterm "vterm")
-
-(defun pjones:project-switch-commands ()
-  "Choose an operation to perform on the current project."
-  (interactive)
-  (pjones:keymap-popup-show 'project-prefix-map))
 
 (defun pjones:project-vterm ()
   "Start a `vterm' for the current project.
@@ -31,7 +27,13 @@ Ensures that the buffer name doesn't change so it can be found again."
       (call-interactively #'project-shell)))
 
 (custom-set-variables
- '(project-switch-commands 'pjones:project-switch-commands))
+ '(project-switch-commands
+   '((project-async-shell-command "Cmd" ?&)
+     (magit-project-status "Magit" ?m)
+     (pjones:project-vterm "Shell" ?s)
+     (project-dired "Dired" ?d)
+     (projectile-find-file "File" ?f)
+     (projectile-run-project "Run" ?r))))
 
 (let ((map project-prefix-map))
   (define-key map (kbd "c") #'pjones:projectile-compile-project)
@@ -39,6 +41,7 @@ Ensures that the buffer name doesn't change so it can be found again."
   (define-key map (kbd "D") #'project-find-dir)
   (define-key map (kbd "f") #'projectile-find-file)
   (define-key map (kbd "m") #'magit-project-status)
+  (define-key map (kbd "M") #'magit-file-dispatch)
   (define-key map (kbd "r") #'projectile-run-project)
   (define-key map (kbd "s") #'pjones:project-vterm)
   (define-key map (kbd "t") #'projectile-test-project))
