ab1eacc729547555bbbff896ed8f6be8f624aa69
Peter Jones <pjones@devalot.com>
2023-08-18 13:32:59 -0700

    org-roam: Improve backlink section in HTML exports
    Only include backlinks in HTML, and only include links to the files,
    not a preview of their content.

--- modes/org-roam-conf.el
+++ modes/org-roam-conf.el
@@ -78,77 +78,74 @@ representation for the files to include, as returned by
   (delete-file (concat org-roam-directory "/sitemap.org")))
 
 ;; Adapted from: https://org-roam.discourse.group/t/export-backlinks-on-org-export/1756/24
-(defun pjones:org-roam-insert-backlinks (&optional _backend)
-  "Insert `org-roam' back links during export."
-  (when-let* ((source-node (org-roam-node-at-point))
-              (source-file (org-roam-node-file source-node))
-              (nodes-in-file (cl-remove-if-not
-                              (lambda (item)
-                                (string-equal (org-roam-node-file item)
-                                              source-file))
-                              (org-roam-node-list)))
-              ;; Nodes don't store the last position, so get the next
-              ;; headline position and subtract one character (or, if
-              ;; no next headline, get point-max)
-              (nodes-and-end
-               (mapcar
-                (lambda (node)
-                  (let* ((start (org-roam-node-point node))
-                         (end (save-excursion
-                                (goto-char start)
-                                (if (org-before-first-heading-p) ;; file node
-                                    (point-max)
-                                  (call-interactively #'org-forward-heading-same-level)
-                                  (if (> (point) start)
-                                      (- (point) 1) ;; successfully found next
-                                    (point-max)))))) ;; there was no next
-                    (cons node end)))
-                nodes-in-file)))
-    (dolist (node-and-end (sort nodes-and-end (lambda (a b) (> (cdr a) (cdr b)))))
-      (when-let* ((node (car node-and-end))
-                  (end-position (cdr node-and-end))
-                  (backlinks (org-roam-backlinks-get node))
-                  (heading (format "\n\n%s Related Notes\n"
-                                   (make-string (+ (org-roam-node-level node) 1) ?*)))
-                  (properties-drawer ":PROPERTIES:\n:HTML_CONTAINER_CLASS: references\n:END:\n"))
-        (goto-char end-position)
-        (insert heading)
-        (insert properties-drawer)
-        (dolist (backlink backlinks)
-          (let* ((source-node (org-roam-backlink-source-node backlink))
-                 (source-file (org-roam-node-file source-node))
-                 (properties (org-roam-backlink-properties backlink))
-                 (outline (when-let ((outline (plist-get properties :outline)))
-                            (when (> (length outline) 1)
-                              (mapconcat #'org-link-display-format outline " > "))))
-                 (point (org-roam-backlink-point backlink))
-                 (text (replace-regexp-in-string
-                        "\n" " " (org-roam-preview-get-contents source-file point)))
-                 (reference (format "%s [[id:%s][%s]]\n%s\n%s\n\n"
-                                    (make-string (+ (org-roam-node-level node) 2) ?*)
-                                    (org-roam-node-id source-node)
-                                    (org-roam-node-title source-node)
-                                    (if outline
-                                        (format
-                                         "%s (/%s/)"
-                                         (make-string (+ (org-roam-node-level node) 3) ?*)
-                                         outline)
-                                      "")
-                                    text))
-                 (label-list (with-temp-buffer
-                               (insert text)
-                               (org-element-map (org-element-parse-buffer) 'footnote-reference
-                                 (lambda (reference)
-                                   (org-element-property :label reference)))))
-                 (footnote-string-list
-                  (with-temp-buffer
-                    (insert-file-contents source-file)
-                    (mapcar (lambda (label) (buffer-substring-no-properties
-                                      (nth 1 (org-footnote-get-definition label))
-                                      (nth 2 (org-footnote-get-definition label))))
-                          label-list))))
-            (mapc (lambda (footnote-string) (insert footnote-string)) footnote-string-list)
-            (insert reference)))))))
+(defun pjones:org-roam-insert-backlinks (backend)
+  "Insert `org-roam' back links during HTML export.
+BACKEND is a symbol indicating the current publishing back-end."
+  (when (eq backend 'html)
+    (when-let* ((source-node (org-roam-node-at-point))
+                (source-file (org-roam-node-file source-node))
+                (nodes-in-file (cl-remove-if-not
+                                (lambda (item)
+                                  (string-equal (org-roam-node-file item)
+                                                source-file))
+                                (org-roam-node-list)))
+                ;; Nodes don't store the last position, so get the next
+                ;; headline position and subtract one character (or, if
+                ;; no next headline, get point-max)
+                (nodes-and-end
+                 (mapcar
+                  (lambda (node)
+                    (let* ((start (org-roam-node-point node))
+                           (end (save-excursion
+                                  (goto-char start)
+                                  (if (org-before-first-heading-p) ;; file node
+                                      (point-max)
+                                    (call-interactively #'org-forward-heading-same-level)
+                                    (if (> (point) start)
+                                        (- (point) 1) ;; successfully found next
+                                      (point-max)))))) ;; there was no next
+                      (cons node end)))
+                  nodes-in-file)))
+      (dolist (node-and-end (sort nodes-and-end (lambda (a b) (> (cdr a) (cdr b)))))
+        (when-let* ((node (car node-and-end))
+                    (end-position (cdr node-and-end))
+                    (backlinks (org-roam-backlinks-get node))
+                    (heading (format "\n\n%s Related Notes\n"
+                                     (make-string (+ (org-roam-node-level node) 1) ?*)))
+                    (properties-drawer ":PROPERTIES:\n:HTML_CONTAINER_CLASS: references\n:END:\n"))
+          (goto-char end-position)
+          (insert heading)
+          (insert properties-drawer)
+          (dolist (backlink backlinks)
+            (let* ((source-node (org-roam-backlink-source-node backlink))
+                   (source-file (org-roam-node-file source-node))
+                   (properties (org-roam-backlink-properties backlink))
+                   (outline (when-let ((outline (plist-get properties :outline)))
+                              (when (> (length outline) 1)
+                                (mapconcat #'org-link-display-format outline " > "))))
+                   (reference (format "%s [[id:%s][%s]]\n%s\n\n"
+                                      (make-string (+ (org-roam-node-level node) 2) ?*)
+                                      (org-roam-node-id source-node)
+                                      (org-roam-node-title source-node)
+                                      (if outline
+                                          (format
+                                           "%s (/%s/)"
+                                           (make-string (+ (org-roam-node-level node) 3) ?*)
+                                           outline)
+                                        "")))
+                   (label-list (with-temp-buffer
+                                 (org-element-map (org-element-parse-buffer) 'footnote-reference
+                                   (lambda (reference)
+                                     (org-element-property :label reference)))))
+                   (footnote-string-list
+                    (with-temp-buffer
+                      (insert-file-contents source-file)
+                      (mapcar (lambda (label) (buffer-substring-no-properties
+                                          (nth 1 (org-footnote-get-definition label))
+                                          (nth 2 (org-footnote-get-definition label))))
+                              label-list))))
+              (mapc (lambda (footnote-string) (insert footnote-string)) footnote-string-list)
+              (insert reference))))))))
 
 ;; Ensure the database is up-to-date:
 (org-roam-db-autosync-mode)
@@ -157,7 +154,7 @@ representation for the files to include, as returned by
 (consult-org-roam-mode)
 
 ;; Hooks:
-(add-hook 'org-export-before-processing-hook #'pjones:org-roam-insert-backlinks)
+(add-hook 'org-export-before-processing-functions #'pjones:org-roam-insert-backlinks)
 (add-hook 'org-roam-find-file-hook #'pjones:org-roam-buffer-name)
 
 ;;; org-roam-conf.el ends here
