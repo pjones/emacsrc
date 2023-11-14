f673b0d4c4d93b977577fe84dab621b9c80243e4
Peter Jones <pjones@devalot.com>
2023-11-14 07:30:58 -0700

    theme: Change the default theme so something more colorful

--- lisp/ui.el
+++ lisp/ui.el
@@ -200,7 +200,7 @@ The mode construct list is then returned."
 ;; Hooks:
 (add-hook 'after-init-hook
   (defun pjones:set-initial-theme ()
-    (pjones:load-theme 'ef-dark)))
+    (pjones:load-theme 'ef-duo-dark)))
 
 (add-hook 'after-init-hook #'pjones:configure-new-frame)
 (add-hook 'after-make-frame-functions #'pjones:configure-new-frame)
