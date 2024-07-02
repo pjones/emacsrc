8775f02a5f608cab636f7d6339a922d10706d0ef
Peter Jones <pjones@devalot.com>
2024-07-02 15:23:58 -0700

    ui: Resume using transparent backgrounds (on Wayland)

--- lisp/ui.el
+++ lisp/ui.el
@@ -150,6 +150,7 @@ If PREV is non-nil go to the previous theme."
       (set-face-attribute 'variable-pitch nil :family "IBM Plex Serif"))))
 
 (add-to-list 'default-frame-alist '(cursor-type  . bar))
+(add-to-list 'default-frame-alist `(alpha-background . ,pjones:frame-alpha-background))
 (setq frame-title-format '(:eval (pjones:frame-title-file-name)))
 
 (defun pjones:find-file-hook ()
