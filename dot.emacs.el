a20e7a2367b8905c6c453f80411862efa6629d53
Peter Jones <pjones@devalot.com>
2024-07-28 12:31:23 -0700

    gnus: Stop creating ugly News directories in $HOME

--- dot.emacs.el
+++ dot.emacs.el
@@ -1,6 +1,21 @@
+;;; dot.emacs.el -- Bootstrap Emacs.
+;;
+;;; Commentary:
+;;
+;; Emacs loads this file, which in turn loads all of my configuration.
+;;
+;;; Code:
+
+;; Silence the linter/compiler:
+(defvar gnus-directory)
+(declare-function pjones:load-configuration-files "./lisp/loadpath")
+
 ;; Important load settings:
-(setq user-emacs-directory (expand-file-name "~/.cache/emacs/"))
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
+      gnus-directory (expand-file-name "~/.cache/emacs/news"))
 
 ;; Load other configuration files
 (load "@loadpathel@")
 (pjones:load-configuration-files)
+
+;;; dot.emacs.el ends here
