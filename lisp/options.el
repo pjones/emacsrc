385b269fced7c972181be223a5976e6784f630de
Peter Jones <pjones@devalot.com>
2024-09-11 13:21:43 -0700

    Switch to new PGP key

--- lisp/options.el
+++ lisp/options.el
@@ -12,7 +12,7 @@
 (custom-set-variables
  '(custom-file (concat user-emacs-directory "custom-init.el"))
  '(disabled-command-function nil)
- '(epa-file-encrypt-to "B3CDF5E4B27790430DACE07F526722D1204284CB")
+ '(epa-file-encrypt-to "4D0CD0756F1B8B9D3DCD0CAAE1CF584F79D0D3DC")
  '(inhibit-eol-conversion t)
  '(make-backup-files nil)
  '(server-client-instructions nil)
