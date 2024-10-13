a4cc9e15365a5ad1bc214b980a3a6e1ba7409d25
Peter Jones <pjones@devalot.com>
2024-10-13 14:12:53 +0200

    space: Better space cycling

--- modes/simple-conf.el
+++ modes/simple-conf.el
@@ -8,6 +8,7 @@
 
 (custom-set-variables
  '(async-shell-command-buffer 'new-buffer)
+ '(cycle-spacing-actions '((just-one-space -) (delete-all-space -) restore))
  '(kill-do-not-save-duplicates t)
  '(next-error-highlight 1)
  '(next-error-message-highlight 'keep)
