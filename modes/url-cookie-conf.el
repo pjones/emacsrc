260847b75dbbdba715681008e20b645d15fe06b0
Peter Jones <pjones@devalot.com>
2025-02-03 16:41:05 +0100

    url: Store cookies in a better place

---
+++ modes/url-cookie-conf.el
@@ -0,0 +1,12 @@
+;;; url-cookie-conf.el -- Settings for `url-cookie' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'url-cookie)
+
+(custom-set-variables
+ '(url-cookie-file (concat user-emacs-directory "url-cookies")))
+
+;;; url-cookie-conf.el ends here
