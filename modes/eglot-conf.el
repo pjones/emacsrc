af219067a1e6ba85de92d2a87c099dc309a1c3c7
Peter Jones <pjones@devalot.com>
2025-02-03 14:47:48 +0100

    eglot: Don't reformat buffer as I type

--- modes/eglot-conf.el
+++ modes/eglot-conf.el
@@ -15,4 +15,10 @@
  'eglot--snippet-expansion-fn
  :override #'pjones:eglot--snippet-expansion-fn)
 
+;; Don't reformat buffers while I'm typing:
+;; https://chaos.social/@root42/113548269273998426
+(add-to-list
+ 'eglot-ignored-server-capabilities
+ :documentOnTypeFormattingProvider)
+
 ;;; eglot-conf.el ends here
