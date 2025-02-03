8ad2deac97fadd9b7c42dcd84156e1455b0e8197
Peter Jones <pjones@devalot.com>
2025-02-03 15:06:39 +0100

    consult: Remove restrictions on buffer switching
    Now that I'm using a single Emacs instance I don't want to restrict
    buffer switching.  If I try to switch to a buffer that is open in
    another frame on another workspace my code prevented the buffer from
    showing up.  I no longer want that.

--- modes/consult-conf.el
+++ modes/consult-conf.el
@@ -4,35 +4,15 @@
 ;;
 ;;; Code:
 
-(eval-when-compile
-  (require 'subr-x))
-
 (require 'consult)
 (require 'project)
-
-(custom-set-variables
- '(consult-project-root-function #'pjones:consult-project-root-function))
-
-(defvar pjones:consult-orig-buffer-items
-  (plist-get consult--source-buffer :items)
-  "Original function for fetching buffer names.")
-
-(defun pjones:consult-source-buffer-items ()
-  "Replacement function for `consult--source-buffer' :items.
-
-This version of the :items function discards buffers that are
-already being shown in a window."
-  (seq-remove
-   (lambda (name-and-buffer) (get-buffer-window (cdr name-and-buffer) 'visible))
-   (funcall pjones:consult-orig-buffer-items)))
-
-;; Update consult--source-buffer with our custom :items function:
-(plist-put consult--source-buffer :items
-           #'pjones:consult-source-buffer-items)
 
 (defun pjones:consult-project-root-function ()
   "Return the current project's root directory."
   (when-let ((project (project-current)))
     (project-root project)))
+
+(custom-set-variables
+ '(consult-project-root-function #'pjones:consult-project-root-function))
 
 ;;; consult-conf.el ends here
