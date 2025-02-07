af2926c932aabec1a3876d352d993a5618856169
Peter Jones <pjones@devalot.com>
2025-02-07 16:18:16 +0100

    plantuml: Try to reuse the same window for previews

--- modes/plantuml-mode-conf.el
+++ modes/plantuml-mode-conf.el
@@ -6,8 +6,23 @@
 
 (require 'plantuml-mode)
 
+(defun pjones:plantuml-preview ()
+  "Wrapper for `plantuml-preview'.
+Forces `plantuml-preview' to reuse the same window."
+  (interactive)
+  (let* ((buffer (get-buffer plantuml-preview-buffer))
+         (window (and buffer (get-buffer-window buffer t))))
+    (when window
+      (display-buffer-override-next-command
+       (lambda (_buffer alist)
+         (window--display-buffer buffer window 'reuse alist)
+         (cons window 'reuse))))
+    (call-interactively #'plantuml-preview)))
+
 (custom-set-variables
  '(plantuml-indent-level 2)
  '(plantuml-default-exec-mode 'executable))
+
+(define-key plantuml-mode-map (kbd "C-c C-c") #'pjones:plantuml-preview)
 
 ;;; plantuml-mode-conf.el ends here
