015849d99a7d3e872814eb277ad443a0dc8472d8
Peter Jones <pjones@devalot.com>
2025-02-16 15:11:08 +0100

    go-translate: Split window the way I want.

--- modes/go-translate-conf.el
+++ modes/go-translate-conf.el
@@ -12,6 +12,9 @@
    (gt-translator
     :taker   (gt-taker :text 'buffer :pick 'paragraph)
     :engines (list (gt-google-engine))
-    :render  (gt-buffer-render))))
+    :render  (gt-buffer-render)))
+ '(gt-buffer-render-window-config
+   '((display-buffer-reuse-window display-buffer-in-direction)
+     (direction . below))))
 
 ;;; go-translate-conf.el ends here
