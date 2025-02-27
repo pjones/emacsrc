ca2c20f7f153e0623d240f374696dcf65fb4cee3
Peter Jones <pjones@devalot.com>
2025-02-27 17:04:35 +0100

    ess: Initial configuration

---
+++ modes/ess-conf.el
@@ -0,0 +1,12 @@
+;;; ess-conf.el -- Settings for `ess' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'ess)
+
+(custom-set-variables
+ '(ess-indent-with-fancy-comments nil))
+
+;;; ess-conf.el ends here
