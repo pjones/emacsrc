6566a58c4a3bb47f4c1f1704891b8c1eca0073a4
Peter Jones <pjones@devalot.com>
2025-04-26 20:37:46 +0200

    which-func: Use the header line instead of the mode line

---
+++ modes/which-func-conf.el
@@ -0,0 +1,12 @@
+;;; which-func-conf.el -- Settings for `which-func' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'which-func)
+
+(custom-set-variables
+ '(which-func-display 'header))
+
+;;; which-func-conf.el ends here
