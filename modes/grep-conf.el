835226d8dc5b3295fb36adefbadbb32dcd1a8ab0
Peter Jones <pjones@devalot.com>
2025-04-27 12:54:52 +0200

    grep: Add configuration

---
+++ modes/grep-conf.el
@@ -0,0 +1,12 @@
+;;; grep-conf.el -- Settings for `grep' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'grep)
+
+(custom-set-variables
+ '(grep-use-headings t))
+
+;;; grep-conf.el ends here
