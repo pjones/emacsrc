35856cc1574a3f6792b2e16e1e93d2d2df35ad9a
Peter Jones <pjones@devalot.com>
2025-05-12 14:55:52 +0200

    dired: Add git clone, lazy load dependencies

--- modes/dired-conf.el
+++ modes/dired-conf.el
@@ -7,14 +7,22 @@
 ;;; Code:
 
 (require 'dired)
-(require 'dired-filter)
-(require 'dired-narrow)
-(require 'dired-subtree)
 (require 'dired-x)
-(require 'noccur)
-(require 'wdired)
 
+;; Make the linter happy:
+(declare-function diff-hl-dired-mode "diff-hl-dired")
+(declare-function dired-filter-mode "dired-filter")
+(declare-function dired-subtree--get-ov "dired-subtree")
+(declare-function dired-subtree-next-sibling "dired-subtree")
+(declare-function dired-subtree-previous-sibling "dired-subtree")
+(declare-function dired-subtree-toggle "dired-subtree")
+(declare-function dired-subtree-up "dired-subtree")
+(declare-function dwim-shell-command "dwim-shell-command")
+(declare-function magit-clone-regular "magit-clone")
+(declare-function noccur-dired "noccur")
 (declare-function org-open-file "org")
+(declare-function wdired-finish-edit "wdired")
+(defvar magit-clone-default-directory)
 
 (defmacro pjones:dired-cwd-do (func)
   "Call FUNC inside the current file's directory."
@@ -23,26 +31,34 @@
      (let ((default-directory (dired-current-directory)))
        (call-interactively ,func))))
 
+(defun pjones:dired-magit-clone-here ()
+  "Use `magit-clone-regular' in the current directory."
+  (interactive)
+  (let ((magit-clone-default-directory (dired-current-directory)))
+    (call-interactively #'magit-clone-regular)))
+
 ;; Settings:
 (custom-set-variables
-  '(dired-auto-revert-buffer t)
-  '(dired-create-destination-dirs 'ask)
-  '(dired-do-revert-buffer t)
-  '(dired-dwim-target t)
-  '(dired-filter-mark-prefix nil)
-  '(dired-filter-prefix nil)
-  '(dired-hide-details-hide-symlink-targets nil)
-  '(dired-isearch-filenames t)
-  '(dired-listing-switches "-lhA --literal --group-directories-first")
-  '(dired-subtree-line-prefix-face 'subtree)
-  '(dired-vc-rename-file t)
-  '(dired-subtree-use-backgrounds nil))
+ '(diff-hl-dired-extra-indicators nil)
+ '(dired-auto-revert-buffer t)
+ '(dired-create-destination-dirs 'ask)
+ '(dired-do-revert-buffer t)
+ '(dired-dwim-target t)
+ '(dired-filter-mark-prefix nil)
+ '(dired-filter-prefix nil)
+ '(dired-hide-details-hide-symlink-targets nil)
+ '(dired-isearch-filenames t)
+ '(dired-listing-switches "-lhA --literal --group-directories-first")
+ '(dired-subtree-line-prefix-face 'subtree)
+ '(dired-subtree-use-backgrounds nil)
+ '(dired-vc-rename-file t))
 
 (let ((map dired-mode-map))
   (define-key map (kbd "!") (pjones:dired-cwd-do #'dwim-shell-command))
   (define-key map (kbd "&") (pjones:dired-cwd-do #'dwim-shell-command))
   (define-key map (kbd "* a") #'pjones:dired-mark-all-files)
   (define-key map (kbd "<return>") #'pjones:dired-insert-or-visit)
+  (define-key map (kbd "c") #'pjones:dired-magit-clone-here)
   (define-key map (kbd "C-c C-c") #'pjones:dired-toggle-wdired)
   (define-key map (kbd "C-c M-w") #'pjones:dired-copy-filename-as-kill)
   (define-key map (kbd "C-c M-W") (pjones:dired-cwd-do 'pjones:kill-directory-name))
@@ -50,7 +66,8 @@
   (define-key map (kbd "C-x C-f") (pjones:dired-cwd-do 'find-file))
   (define-key map (kbd "M-n") #'dired-subtree-next-sibling)
   (define-key map (kbd "M-p") #'pjones:dired-subtree-up-or-prev)
-  (define-key map (kbd "M-s o") #'noccur-dired))
+  (define-key map (kbd "M-s o") #'noccur-dired)
+  (define-key map (kbd "X") #'dired-do-compress-to))
 
 (defun pjones:dired-subtree-up-or-prev (&optional arg)
   "Move point up a directory, or to the previous sibling.
@@ -58,6 +75,7 @@ When in a nested directory, move up to the parent.  If at the top of
 the hierarchy then move to the previous line.
 ARG is the number of lines to jump."
   (interactive "p")
+  (require 'dired-subtree)
   (if-let (ov (dired-subtree--get-ov))
       (dired-subtree-up arg)
     (dired-subtree-previous-sibling arg)))
@@ -75,6 +93,7 @@ ARG is the number of lines to jump."
 If point is on a directory, insert that directory into the current
 Dired buffer.  Otherwise visit the file under point."
   (interactive)
+  (require 'dired-subtree)
   (let ((name (dired-get-file-for-visit)))
     (if (file-directory-p name) (dired-subtree-toggle)
       (require 'org)
@@ -96,6 +115,7 @@ Dired buffer.  Otherwise visit the file under point."
 (defun pjones:dired-toggle-wdired ()
   "Toggle `wdired-mode'."
   (interactive)
+  (require 'wdired)
   (if (eq major-mode 'wdired-mode)
       (wdired-finish-edit)
     (wdired-change-to-wdired-mode)))
