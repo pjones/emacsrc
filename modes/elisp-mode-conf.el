ff9d66371a3b94ca3b9b99ba71a2d86115dab142
Peter Jones <pjones@devalot.com>
2025-05-18 18:09:27 +0200

    flymake: Override a stupid default
    Flymake doesn't use `load-path`, which means it can't find Emacs
    packages when checking my config files.  Pretty dumb.  (Maybe just
    overly cautious.)

---
+++ modes/elisp-mode-conf.el
@@ -0,0 +1,12 @@
+;;; elisp-mode-conf.el -- Settings for `elisp-mode' -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'elisp-mode)
+
+(custom-set-variables
+ '(elisp-flymake-byte-compile-load-path load-path)) ; Stupid default
+
+;;; elisp-mode-conf.el ends here
