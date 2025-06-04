0524fa0dab0a15ef2b94ca8defa52f5ef8b203e7
Peter Jones <pjones@devalot.com>
2025-06-04 15:17:24 +0200

    treesit: CSharp tree sitter mode has been fixed

--- modes/treesit-auto-conf.el
+++ modes/treesit-auto-conf.el
@@ -7,8 +7,7 @@
 (require 'treesit-auto)
 
 ;; Don't use tree sitter for the following modes:
-(dolist (mode '(bash        ; Bad syntax highlighting.
-                c-sharp))   ; Worse syntax highlighting!
+(dolist (mode '(bash))        ; Bad syntax highlighting.
   (delete mode treesit-auto-langs))
 
 ;;; treesit-auto-conf.el ends here
