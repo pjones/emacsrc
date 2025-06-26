15968e799fc9df1bbd9f95f10eeefdbf471cc6b1
Peter J. Jones <peter@jonesbunch.com>
2025-06-26 16:56:27 +0200

    org-grader: Update and add post-template hook

--- modes/org-grader-conf.el
+++ modes/org-grader-conf.el
@@ -6,10 +6,21 @@
 
 (require 'org-grader)
 
+(declare-function pjones:org-hide-others "./org-conf")
+(defvar org-grader-template-point)
+
 (defun pjones:org-grader-mode-hook ()
   "Hook for `org-grader-mode'."
   (keymap-local-set "C-c i" #'org-grader-template-insert))
 
+(defun pjones:org-grader-after-insert-hook ()
+  "Hook for `org-grader-after-insert-hook'."
+  (org-narrow-to-subtree)
+  (when org-grader-template-point
+    (goto-char org-grader-template-point))
+  (pjones:org-hide-others))
+
 (add-hook 'org-grader-mode-hook #'pjones:org-grader-mode-hook)
+(add-hook 'org-grader-after-insert-hook #'pjones:org-grader-after-insert-hook)
 
 ;;; org-grader-conf.el ends here
