a42d613d77017a7575e96dd09409a6a79ee3669a
Peter J. Jones <peter@jonesbunch.com>
2025-07-03 15:59:45 +0200

    org-grader: Update to latest version, don't narrow
    Turns out I want to insert a bunch of templates at once so narrowing
    creates more work for me.

--- modes/org-grader-conf.el
+++ modes/org-grader-conf.el
@@ -7,20 +7,13 @@
 (require 'org-grader)
 
 (declare-function pjones:org-hide-others "./org-conf")
+(defvar org-grader-mode-map)
 (defvar org-grader-template-point)
 
 (defun pjones:org-grader-mode-hook ()
   "Hook for `org-grader-mode'."
-  (keymap-local-set "C-c i" #'org-grader-template-insert))
-
-(defun pjones:org-grader-after-insert-hook ()
-  "Hook for `org-grader-after-insert-hook'."
-  (org-narrow-to-subtree)
-  (when org-grader-template-point
-    (goto-char org-grader-template-point))
-  (pjones:org-hide-others))
+  (keymap-set org-grader-mode-map "C-c i" #'org-grader-template-insert))
 
 (add-hook 'org-grader-mode-hook #'pjones:org-grader-mode-hook)
-(add-hook 'org-grader-after-insert-hook #'pjones:org-grader-after-insert-hook)
 
 ;;; org-grader-conf.el ends here
