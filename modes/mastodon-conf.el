0295c597ff89e253f816217afcc9ca23ff712af9
Peter J. Jones <peter@jonesbunch.com>
2025-07-08 16:46:45 +0200

    mastodon: auth-source doesn't work, toot with visual-line
    I think because I use pass, auth-source silently fails.  Either way,
    mastodon.el reports that I need to authenticate each time I start it.
    Setting this variable to nil resolves that.
    
    Also, enable visual-line-mode for composing toots to make things look
    a bit nicer.

--- modes/mastodon-conf.el
+++ modes/mastodon-conf.el
@@ -6,9 +6,19 @@
 
 (require 'mastodon)
 
+(declare-function visual-fill-mode "visual-fill")
+
 ;; https://codeberg.org/martianh/mastodon.el
 (custom-set-variables
+ '(mastodon-auth-use-auth-source nil)
  '(mastodon-instance-url "https://hostux.social/")
  '(mastodon-active-user "devalot"))
+
+(defun pjones:mastodon-toot-mode-hook ()
+  "Hook for `mastodon-toot-mode'."
+  (auto-fill-mode -1)
+  (visual-line-mode))
+
+(add-hook 'mastodon-toot-mode-hook #'pjones:mastodon-toot-mode-hook)
 
 ;;; mastodon-conf.el ends here
