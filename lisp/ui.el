a05e14ddb1fda5d82156d275f0d25c52a24e581b
Peter J. Jones <peter@jonesbunch.com>
2025-08-18 10:50:45 +0200

    ui: Revert to a more complete and usable theme

--- lisp/ui.el
+++ lisp/ui.el
@@ -10,7 +10,7 @@
 
 (declare-function consult-theme "consult")
 
-(defvar pjones:dark-theme 'timu-rouge
+(defvar pjones:dark-theme 'ef-maris-dark
   "Default dark theme.")
 
 (defvar pjones:light-theme 'ef-maris-light
