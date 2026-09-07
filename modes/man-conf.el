0ade2d925eafca67f671e966e94acd599170bfc9
Peter J. Jones <peter@jonesbunch.com>
2026-09-07 15:42:50 +0200

    man: Remove the new hyperlink OSC escape codes

--- modes/man-conf.el
+++ modes/man-conf.el
@@ -3,9 +3,19 @@
 ;;; Commentary:
 ;;
 ;;; Code:
+
+(require 'ansi-osc)
 (require 'man)
+
+(defun pjones:man-cleanup ()
+  "Clean up man pages."
+  (save-excursion
+    ;; https://www.mail-archive.com/bug-groff@gnu.org/msg13845.html
+    (ansi-osc-filter-region (point-min) (point-max))))
 
 (custom-set-variables
  '(Man-notify-method 'pushy))
+
+(add-hook 'Man-cooked-hook #'pjones:man-cleanup)
 
 ;;; man-conf.el ends here
