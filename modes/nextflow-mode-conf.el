e8fc993e21bc8ffe53c4b7523609024b0450a7df
Peter J. Jones <peter@jonesbunch.com>
2026-09-08 17:19:52 +0200

    nextflow: Configure the LSP server

--- modes/nextflow-mode-conf.el
+++ modes/nextflow-mode-conf.el
@@ -4,6 +4,7 @@
 ;;
 ;;; Code:
 
+(require 'eglot)
 (require 'nextflow-mode)
 (require 'nextflow-ts-mode)
 (require 'reformatter)
@@ -18,16 +19,33 @@
 
 (defun pjones:nextflow-mode-hook ()
   "Hook for `nextflow-mode-hook'."
-  (if (eq 'nextflow-mode major-mode)
+  (if (eq major-mode 'nextflow-mode)
       (nextflow-ts-mode)
-    (nextflow-format-on-save-mode)))
+    (let* ((project (project-current))
+           (dir (or (and project (project-root project))
+                    default-directory)))
+      (dir-locals-set-directory-class dir 'nextflow-project)
+      (eglot-ensure)
+      (eglot-semantic-tokens-mode -1)
+      (nextflow-format-on-save-mode))))
 
-;; Nextflow Language Server currently doesn't work with eglot :(
-;; https://github.com/nextflow-io/language-server/issues/118
-;; (add-to-list
-;;  'eglot-server-programs
-;;  '((nextflow-mode nextflow-ts-mode) .
-;;      ("nextflow-language-server")))
+;; These variable need to be set for nextflow to work correctly:
+(let ((variables '((eglot-workspace-configuration
+                 . (:nextflow
+                    (:debug :json-false
+                            :files (:exclude [".git" ".nf-test" "work"])
+                            :formatting (:harshilAlignment :json-false)
+                            :suppressFutureWarnings t))))))
+  (dir-locals-set-class-variables
+   'nextflow-project
+   `((nextflow-mode . ,variables)
+     (nextflow-ts-mode . ,variables))))
+
+;; Ensure eglot can find the LSP server:
+(add-to-list 'eglot-server-programs
+             '(((nextflow-ts-mode :language-id "nextflow")
+                (nextflow-mode :language-id "nextflow"))
+               . ("nextflow-language-server")))
 
 (add-to-list 'nextflow-mode-hook #'pjones:nextflow-mode-hook)
 (add-to-list 'nextflow-ts-mode-hook #'pjones:nextflow-mode-hook)
